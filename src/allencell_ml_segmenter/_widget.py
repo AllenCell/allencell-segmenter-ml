cecf4a9846e4848753788854ccbcae25afeaf2a8
yrkim98 <yrkim98@uw.edu>
2023-03-15 11:36:50 -0700

    include makefile

--- src/allencell_ml_segmenter/_widget.py
+++ src/allencell_ml_segmenter/_widget.py
@@ -6,13 +6,9 @@ see: https://napari.org/stable/plugins/guides.html?#widgets
 
 Replace code below according to your needs.
 """
-from typing import TYPE_CHECKING
-
 from magicgui import magic_factory
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
-
-if TYPE_CHECKING:
-    import napari
+import napari
 
 
 class ExampleQWidget(QWidget):
