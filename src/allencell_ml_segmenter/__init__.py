8cb23ab8b6a1a0e103dd571e4a1ef8c80220ae42
Brian Kim <brian.kim@alleninstitute.org>
2023-04-25 11:55:51 -0700

    basic structure

--- src/allencell_ml_segmenter/__init__.py
+++ src/allencell_ml_segmenter/__init__.py
@@ -1,9 +1,9 @@
 __version__ = "0.0.9"
 
-from ._reader import napari_get_reader
-from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, example_magic_widget
-from ._writer import write_multiple, write_single_image
+from allencell_ml_segmenter.napari.napari_reader import napari_get_reader
+from allencell_ml_segmenter.napari.sample_data import make_sample_data
+from allencell_ml_segmenter.view.example_q_widget import ExampleQWidget, example_magic_widget
+from allencell_ml_segmenter.napari.napari_writer import write_multiple, write_single_image
 
 __all__ = (
     "napari_get_reader",
