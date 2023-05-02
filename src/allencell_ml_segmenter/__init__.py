3289272fbf3efec60181accf95475d5441c40e22
yrkim98 <yrkim98@uw.edu>
2023-05-02 16:38:18 -0700

    cleanup

--- src/allencell_ml_segmenter/__init__.py
+++ src/allencell_ml_segmenter/__init__.py
@@ -2,14 +2,17 @@ __version__ = "0.0.9"
 
 from allencell_ml_segmenter.napari.napari_reader import napari_get_reader
 from allencell_ml_segmenter.napari.sample_data import make_sample_data
-from allencell_ml_segmenter.view.example_q_widget import ExampleQWidget, example_magic_widget
 from allencell_ml_segmenter.napari.napari_writer import write_multiple, write_single_image
+
+from allencell_ml_segmenter.view.exmaple_view import ExampleView
+
+from allencell_ml_segmenter.controller.example_controller import ExampleController
 
 __all__ = (
     "napari_get_reader",
     "write_single_image",
     "write_multiple",
     "make_sample_data",
-    "ExampleQWidget",
-    "example_magic_widget",
+    "ExampleView",
+    "ExampleController",
 )
