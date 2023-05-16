a6617e3e2bf1ddcd7bfc14b8359ec35994b08fef
yrkim98 <yrkim98@uw.edu>
2023-05-16 09:29:26 -0700

    able to change via router

---
+++ src/allencell_ml_segmenter/view/test_view.py
@@ -0,0 +1,18 @@
+from allencell_ml_segmenter.core.view import View
+from allencell_ml_segmenter.view.test_widget import TestWidget
+from ._main_template import MainTemplate
+from qtpy.QtWidgets import QVBoxLayout
+
+class TestView(View):
+    def __init__(self):
+        # hook up controller here when ready
+        super().__init__(template_class=MainTemplate)
+
+    def load(self, model):
+        print("loaded")
+
+    def _setup_ui(self):
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+        layout.addWidget(TestWidget())
