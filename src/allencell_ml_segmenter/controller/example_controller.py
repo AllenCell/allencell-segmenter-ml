4b15bbeebc7d4b5e288792b8727c0812985718c4
Brian Kim <brian.kim@alleninstitute.org>
2023-05-18 15:49:52 -0700

    subscriber pattern conversion, proof of concept

--- src/allencell_ml_segmenter/controller/example_controller.py
+++ src/allencell_ml_segmenter/controller/example_controller.py
@@ -1,18 +1,23 @@
-from qtpy.QtWidgets import QWidget
-import napari
-import hydra
-from omegaconf import OmegaConf
-from aics_im2im.train import entry_point_call
-from hydra.core.global_hydra import GlobalHydra
-from hydra.core.hydra_config import HydraConfig
 from allencell_ml_segmenter.view.test_view import TestView
+from allencell_ml_segmenter.model.pub_sub import Subscriber, Event
+from allencell_ml_segmenter.model.test_model import TestModel
 
-class TestController():
-    def __init__(self, application) -> None:
+
+class UiController(Subscriber):
+    def __init__(self, application, model: TestModel) -> None:
+        super().__init__()
         # add all ui elements here
         self.application = application
         self._view: TestView = TestView()
-        # called on init
+        self._model: TestModel = model
+        self._model.subscribe(self)
+
+    def handle_event(self, event: Event):
+        #TODO change to switch
+        if event == Event.TRAINING:
+            self._view.widget.label.setText(f"training is running {self._model.get_model_training()}")
+            # set training label text method in view
+            # mock view and see if method called
 
     def index(self):
         # called when loading new controller
@@ -23,7 +28,7 @@ class TestController():
         print("napari has", len(self.viewer.layers), "layers")
 
     def _connect_slots(self):
-        self._view.widget.btn.clicked.connect(self.start_training_test)
+        self._view.widget.btn.clicked.connect(self.change_label)
 
     def load_view(self):
         """
@@ -32,13 +37,12 @@ class TestController():
         """
         return self.application.view_manager.load_view(self._view)
 
-    def start_training_test(self):
-        GlobalHydra.instance().clear()
-        hydra.initialize(version_base="1.3", config_path="../../../../aics-im2im/configs")
+    def change_label(self):
+        self._model.set_model_training(not self._model.get_model_training())
 
-        cfg = hydra.compose(config_name="train", overrides=["trainer=cpu", "experiment=im2im/segmentation.yaml", "hydra.runtime.cwd=."], return_hydra_config=True)
-        HydraConfig().cfg = cfg
-        # OmegaConf.resolve(cfg)
-        entry_point_call(cfg)
-        print(cfg.paths.output_dir)
+
+
+
+
+
 
