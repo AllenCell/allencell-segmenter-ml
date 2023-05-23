d6d644a14023ed3bf9c3b7b9c0f001bf5566ce41
yrkim98 <yrkim98@uw.edu>
2023-05-23 12:38:07 -0700

    test pub sub

--- src/allencell_ml_segmenter/_tests/view/test_sample_view.py
+++ src/allencell_ml_segmenter/_tests/view/test_sample_view.py
@@ -5,6 +5,8 @@ from allencell_ml_segmenter.model.sample_model import SampleModel, Event
 from allencell_ml_segmenter.core.view import View
 from allencell_ml_segmenter.view.sample_view import SampleViewController
 
+from qtpy.QtWidgets import QVBoxLayout
+
 @pytest.fixture
 def sample_model():
     return SampleModel()
@@ -25,4 +27,16 @@ def test_handle_event_training(sample_view_controller, sample_model):
 
     expected_label_text = f"training is running {sample_model.get_model_training()}"
     mock_set_label_text.assert_called_once_with(expected_label_text)
+
+def test_change_label(sample_view_controller, sample_model):
+    assert sample_model.get_model_training() == False
+
+    sample_view_controller.change_label()
+    assert sample_model.get_model_training() == True
+
+def test_load_and_setup_ui(sample_view_controller):
+    sample_view_controller.load()
+    assert isinstance(sample_view_controller.layout(), QVBoxLayout)
+    assert isinstance(sample_view_controller.widget, SampleWidget)
+
 
