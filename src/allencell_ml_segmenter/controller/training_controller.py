7284105d0dde0d856fd4e39c4aea2c66f6519090
yrkim98 <yrkim98@uw.edu>
2023-05-24 13:30:05 -0700

    comment out training service

--- src/allencell_ml_segmenter/controller/training_controller.py
+++ src/allencell_ml_segmenter/controller/training_controller.py
@@ -1,6 +1,6 @@
 from allencell_ml_segmenter.model.publisher import Subscriber, Event
 from allencell_ml_segmenter.model.training_model import TrainingModel
-from allencell_ml_segmenter.services.training_service import TrainingService
+# from allencell_ml_segmenter.services.training_service import TrainingService
 
 
 class TrainingController(Subscriber):
@@ -8,16 +8,16 @@ class TrainingController(Subscriber):
         self,
         application,
         model: TrainingModel,
-        training_service: TrainingService,
+        # training_service: TrainingService,
     ):
         super().__init__()
         self._model = model
         self._model.subscribe(self)
         self._application = application
-        self._training_service = training_service
+        # self._training_service = training_service
 
     def handle_event(self, event: Event):
         # TODO change to switch
         if event == Event.TRAINING:
             if self._model.get_model_training():
-                self._training_service.start_training()
+                pass
