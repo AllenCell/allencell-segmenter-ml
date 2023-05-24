10cfb96f1bb918ddf8ba53998d4dd7d916c667d2
yrkim98 <yrkim98@uw.edu>
2023-05-24 14:39:42 -0700

    resolve pr comments

--- src/allencell_ml_segmenter/_tests/model/test_training_model.py
+++ src/allencell_ml_segmenter/_tests/model/test_training_model.py
@@ -1,15 +1,7 @@
 import pytest
 from allencell_ml_segmenter.model.training_model import TrainingModel
 from allencell_ml_segmenter.model.publisher import Event
-
-
-class MockSubscriber:
-    def __init__(self):
-        self.event_received = None
-
-    def handle_event(self, event):
-        self.event_received = event
-
+from allencell_ml_segmenter._tests.fakes.fake_subscriber import FakeSubscriber
 
 @pytest.fixture
 def sample_model():
@@ -17,13 +9,13 @@ def sample_model():
 
 
 def test_set_model_training(sample_model):
-    subscriber = MockSubscriber()
+    subscriber = FakeSubscriber()
     sample_model.subscribe(subscriber)
 
     sample_model.set_model_training(True)
 
     assert sample_model._model_training
-    assert subscriber.event_received == Event.TRAINING
+    assert subscriber.handled_event == Event.TRAINING
 
 
 def test_get_model_training(sample_model):
