e173044828a4d98eecad07508848091e64526400
yrkim98 <yrkim98@uw.edu>
2023-06-01 12:13:01 -0700

    use stackedwidget to switch pages

--- src/allencell_ml_segmenter/widgets/selection_widget.py
+++ src/allencell_ml_segmenter/widgets/selection_widget.py
@@ -5,23 +5,29 @@ from qtpy.QtWidgets import (
     QSizePolicy,
 )
 
+from allencell_ml_segmenter.model.main_model import MainModel, Page
 
 class SelectionWidget(QWidget):
     """
     A sample widget with two buttons for selecting between training and prediction views.
     """
 
-    def __init__(self):
+    def __init__(self, model: MainModel):
         super().__init__()
-        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.MinimumExpanding)
+        # self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.MinimumExpanding)
         self.setLayout(QVBoxLayout())
         self.layout().setContentsMargins(0, 0, 0, 0)
 
         # Controller
         self.training_button = QPushButton("Training View")
-        self.training_button.clicked.connect(self.start_training)
+        self.training_button.clicked.connect(lambda: self.model.set_current_page(Page.TRAINING))
         self.prediction_button = QPushButton("Prediction View")
+        self.prediction_button.clicked.connect(lambda: self.model.set_current_page(Page.PREDICTION))
 
         # add buttons
         self.layout().addWidget(self.training_button)
         self.layout().addWidget(self.prediction_button)
+
+        # model
+        self.model = model
+
