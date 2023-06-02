f99dcea71dd74d3a7a98d1b1028632e6db8609f6
yrkim98 <yrkim98@uw.edu>
2023-06-01 17:15:07 -0700

    working followup

--- src/allencell_ml_segmenter/view/training_view_controller.py
+++ src/allencell_ml_segmenter/view/training_view_controller.py
@@ -1,12 +1,12 @@
 from allencell_ml_segmenter.core.view import View
-from allencell_ml_segmenter.widgets.sample_widget import SampleWidget
+from allencell_ml_segmenter.widgets.training_widget import TrainingWidget
 from qtpy.QtWidgets import QVBoxLayout
 from allencell_ml_segmenter.model.training_model import Event, TrainingModel
 from allencell_ml_segmenter.model.publisher import Subscriber
 from allencell_ml_segmenter.model.main_model import MainModel
 
 
-class SampleViewController(View, Subscriber):
+class TrainingViewController(View, Subscriber):
     """
     ViewController for SampleWidget, responsible for handling events and updating the model + UI.
     """
@@ -19,37 +19,31 @@ class SampleViewController(View, Subscriber):
 
         # models
         self._main_model = main_model
-        self._model = TrainingModel()
-        self.model.subscribe(self)
+        # self._model = TrainingModel()
+        self._main_model.subscribe(self)
 
         # init widget and connect slots
-        widget = SampleWidget()
+        widget = TrainingWidget()
         widget.connectSlots([self.change_label, self.back_to_main])
         layout.addWidget(widget)
-
-    @property
-    def model(self) -> TrainingModel:
-        """
-        Model property
-        """
-        return self._model
 
     def handle_event(self, event: Event) -> None:
         """
         Handles Events from the Training Model
         """
-        print("sampleviewcontroller handle event called")
+        print("recieved event in training view controller")
+        if event == Event.TRAINING_SELECTED:
+            self._main_model.set_current_view(self)
 
     def change_label(self) -> None:
         """
         Updates model in order to change label in UI
         """
         print("change label called")
-        self._model.set_model_training(not self._model.get_model_training())
+        # self._model.set_model_training(not self._model.get_model_training())
 
     def back_to_main(self) -> None:
         """
         Updates model in order to change page back to main.
         """
-        # print("back to main called")
-        # self._main_model.set_current_view(Page.MAIN)
+        self._main_model.dispatch(Event.MAIN_SELECTED)
