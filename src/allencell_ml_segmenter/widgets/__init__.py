f5aeeaa585e88a94f5c42af1c0655d41266ff72e
yrkim98 <yrkim98@uw.edu>
2023-06-02 17:43:17 -0700

    final

--- src/allencell_ml_segmenter/widgets/__init__.py
+++ src/allencell_ml_segmenter/widgets/__init__.py
@@ -1,9 +1,7 @@
 from allencell_ml_segmenter.widgets.training_widget import TrainingWidget
-from allencell_ml_segmenter.widgets.main_widget import MainWidget
 from allencell_ml_segmenter.widgets.selection_widget import SelectionWidget
 
 __all__ = (
     "TrainingWidget",
-    "MainWidget",
     "SelectionWidget",
 )
