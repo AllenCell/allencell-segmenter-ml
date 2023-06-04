5c3ded48622ed4ff9aec2406d391123fd5ff69e2
yrkim98 <yrkim98@uw.edu>
2023-06-03 19:31:24 -0700

    fix

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch -f --env-filter '
+
+OLD_EMAIL="“yrkim98@uw.edu”"
+CORRECT_NAME="yrkim98"
+CORRECT_EMAIL="yrkim98@uw.edu"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
