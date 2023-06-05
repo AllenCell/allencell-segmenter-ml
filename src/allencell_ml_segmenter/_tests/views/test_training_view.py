e9fc9ca7221f51c11df4fe029423839589eac20b
yrkim98 <yrkim98@uw.edu>
2023-06-05 13:38:14 -0700

    remove changes to init + imports, making explicit imports again

--- src/allencell_ml_segmenter/_tests/views/test_training_view.py
+++ src/allencell_ml_segmenter/_tests/views/test_training_view.py
@@ -2,7 +2,7 @@ import pytest
 from unittest.mock import Mock
 from allencell_ml_segmenter.models.main_model import MainModel
 from allencell_ml_segmenter.core.publisher import Event
-from allencell_ml_segmenter.view.training_view import TrainingView
+from allencell_ml_segmenter.views.training_view import TrainingView
 from allencell_ml_segmenter.widgets.training_widget import TrainingWidget
 
 
--- src/allencell_ml_segmenter/widgets/__init__.py
+++
@@ -1,7 +0,0 @@
-from allencell_ml_segmenter.widgets.training_widget import TrainingWidget
-from allencell_ml_segmenter.widgets.selection_widget import SelectionWidget
-
-__all__ = (
-    "TrainingWidget",
-    "SelectionWidget",
-)
