f99fd646d8733031d0828514aa89fb15b4dbc2b1
William Lee <williamlee0416@gmail.com>
2023-06-13 16:50:58 -0700

    Refactor Example Model

--- src/allencell_ml_segmenter/models/example_model.py
+++ src/allencell_ml_segmenter/models/example_model.py
@@ -1,5 +1,7 @@
 from allencell_ml_segmenter.core.publisher import Publisher
 from allencell_ml_segmenter.core.event import Event
+import os, yaml
+from typing import Dict, Any
 
 
 class ExampleModel(Publisher):
@@ -9,35 +11,35 @@ class ExampleModel(Publisher):
 
     def __init__(self):
         super().__init__()
-        # Current state of example model usage
+
+        # Current save state
         self.saved: bool = False
-        self._model_in_use: bool = False
 
-        # Set fields related to widget
-        # TODO: edit in future commit when I implement the model (and add typing)
-        self.text, self.option, self.choice, self.slider = (
-            None,
-            None,
-            None,
-            None,
-        )
+        # Set default values for fields related to widget
+        self.text: str = ""
+        self.option: str = "One"
+        self.choice: int = None
+        self.slider: int = 0
 
-        # TODO: read in test.yaml and figure out the true index
-        self.index = 0
+        # Read in test.yaml and figure out the proper index
+        if not os.path.isfile("./test.yaml"):
+            self.index: int = 0
+        else:
+            with open("./test.yaml", "r") as file:
+                previous_entries: Dict[int, Dict[str, Any]] = yaml.safe_load(
+                    file
+                )
+                self.index: int = len(previous_entries)
 
-    def get_model_usage_state(self) -> bool:
+    def get_model_save_state(self) -> bool:
         """
-        Getter to get the current state of the model's usage
+        Getter to get the current save state of the model.
         """
-        return self._model_in_use
-
-    def set_example_model(self, running: bool) -> None:
-        """
-        Setter to set the current state of the model.
-        """
-        self._model_in_use = running
-        self.dispatch(Event.EXAMPLE)
+        return self.saved
 
     def save(self, save_status: bool) -> None:
+        """
+        Setter to set the current save state and dispatch a save event to subs.
+        """
         self.saved = save_status
         self.dispatch(Event.SAVE)
