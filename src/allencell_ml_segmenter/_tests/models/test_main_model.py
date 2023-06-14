495cbe22bf0e8a9b0d15df578b53d5c0eaaa05bc
chrishu <chrishu@alleninstitute.org>
2023-06-14 18:19:03 -0400

    refactorign main package

--- src/allencell_ml_segmenter/_tests/models/test_main_model.py
+++ src/allencell_ml_segmenter/_tests/models/test_main_model.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import Mock
 from allencell_ml_segmenter.views.view import View
-from allencell_ml_segmenter.models.main_model import MainModel
+from allencell_ml_segmenter.main.main_model import MainModel
 from allencell_ml_segmenter.core.publisher import Event
 from allencell_ml_segmenter._tests.fakes.fake_subscriber import FakeSubscriber
 
