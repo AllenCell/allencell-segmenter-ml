08b8e31da4c8a1a06bb01e8b10d1fe42df477d75
chrishu <chrishu@alleninstitute.org>
2023-06-15 13:57:56 -0400

    sample service, error message wired, refactoring

--- src/allencell_ml_segmenter/main/main_model.py
+++ src/allencell_ml_segmenter/main/main_model.py
@@ -12,8 +12,6 @@ class MainModel(Publisher):
         super().__init__()
         # Current page of the UI
         self._current_view: View = None
-        self.training_running: bool = False
-        self.perdictions_running: bool = False
 
     def get_current_view(self) -> bool:
         """
@@ -28,28 +26,3 @@ class MainModel(Publisher):
         self._current_view = view
         self.dispatch(Event.ACTION_CHANGE_VIEW)
 
-    def get_training_running(self) -> bool:
-        """
-        getter/property for training running
-        """
-        return self.training_running
-    
-    def set_training_running(self, training_running: bool):     
-        """
-        Set the training running in the UI and dispatch a MainEvent
-        """
-        self.training_running = training_running
-        self.dispatch(Event.PROCESS_TRAINING)   
-
-    def get_perdictions_running(self) -> bool:   
-        """
-        getter/property for perdictions running
-        """
-        return self.perdictions_running
-    
-    def set_perdictions_running(self, perdictions_running: bool):   
-        """
-        Set the perdictions running in the UI and dispatch a MainEvent
-        """
-        self.perdictions_running = perdictions_running
-        self.dispatch(Event.PREDICTIONS)
