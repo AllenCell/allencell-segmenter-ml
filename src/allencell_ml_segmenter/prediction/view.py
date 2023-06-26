3e7aabae396704a2f838dc934f5544e233f47b03
William Lee <williamlee0416@gmail.com>
2023-06-26 14:07:39 -0700

    Create prediction view

---
+++ src/allencell_ml_segmenter/prediction/view.py
@@ -0,0 +1,40 @@
+from allencell_ml_segmenter.core.event import Event
+from allencell_ml_segmenter.core.subscriber import Subscriber
+from allencell_ml_segmenter.main.main_model import MainModel
+from allencell_ml_segmenter.views.view import View
+from allencell_ml_segmenter.prediction.model_input_widget import ModelInputWidget
+from qtpy.QtWidgets import QVBoxLayout, QSizePolicy, QPushButton
+
+
+class PredictionView(View, Subscriber):
+    """
+    Holds the image and model input widgets for prediction.
+    """
+
+    def __init__(self, main_model: MainModel):
+        super().__init__()
+
+        self._main_model: MainModel = main_model
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+        self.layout().setContentsMargins(0, 0, 0, 0)
+        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.MinimumExpanding)
+        self.setLayout(QVBoxLayout())
+
+        model_input_widget: ModelInputWidget = ModelInputWidget()
+        self.layout().addWidget(model_input_widget)
+
+        self._return_btn: QPushButton = QPushButton("Return")
+        self._return_btn.clicked.connect(lambda: self._main_model.dispatch(Event.VIEW_SELECTION_MAIN))
+        self.layout().addWidget(self._return_btn)
+
+        self._main_model.subscribe(
+            Event.VIEW_SELECTION_PREDICTION,
+            self,
+            lambda e: self._main_model.set_current_view(self),
+        )
+
+    def handle_event(self, event: Event) -> None:
+        pass
