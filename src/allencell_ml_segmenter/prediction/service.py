f32828c6c4b823e429b56972e123605cf6bcad22
William Lee <williamlee0416@gmail.com>
2023-06-26 15:46:27 -0700

    Link model, service, and view

---
+++ src/allencell_ml_segmenter/prediction/service.py
@@ -0,0 +1,26 @@
+from allencell_ml_segmenter.core.event import Event
+from allencell_ml_segmenter.core.subscriber import Subscriber
+from allencell_ml_segmenter.prediction.model import PredictionModel
+
+
+class ModelFileService(Subscriber):
+    """
+    Parses the chosen model file to extract the preprocessing method.
+    """
+
+    def __init__(self, model: PredictionModel):
+        super().__init__()
+        self._model = model
+
+        self._model.subscribe(
+            Event.ACTION_PREDICTION_MODEL_FILE_SELECTED, self, lambda e: self.extract_preprocessing_method()
+        )
+
+    def handle_event(self, event: Event) -> None:
+        pass
+
+    def extract_preprocessing_method(self) -> None:
+        """
+        Calls the prediction model's setter for the preprocessing method. Currently set up with a dummy value.
+        """
+        self._model.set_preprocessing_method("foo")
