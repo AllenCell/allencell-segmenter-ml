66ef0fc5cdbc6c72e82db36c31d035941838073b
William Lee <williamlee0416@gmail.com>
2023-06-29 15:44:17 -0700

    Functionality and test for choosing type of auto threshold

---
+++ src/allencell_ml_segmenter/prediction/_tests/test_prediction_model.py
@@ -0,0 +1,62 @@
+import pytest
+
+from allencell_ml_segmenter.main.main_model import MainModel
+from allencell_ml_segmenter.prediction.model_input_widget import (
+    ModelInputWidget,
+)
+from allencell_ml_segmenter.prediction.view import PredictionView
+
+
+@pytest.fixture
+def main_model():
+    return MainModel()
+
+
+@pytest.fixture
+def prediction_view(main_model, qtbot):
+    return PredictionView(main_model)
+
+
+def test_postprocessing_method(prediction_view):
+    # ARRANGE
+    model_input_widget: ModelInputWidget = ModelInputWidget(
+        prediction_view._prediction_model
+    )
+
+    # ACT
+    model_input_widget.top_radio_button_slot()
+
+    # ASSERT
+    assert (
+        prediction_view._prediction_model.get_postprocessing_method()
+        == "simple threshold cutoff"
+    )
+
+    # ACT
+    model_input_widget.mid_radio_button_slot()
+
+    # ASSERT
+    assert (
+        prediction_view._prediction_model.get_postprocessing_method()
+        == "auto threshold"
+    )
+
+
+def test_postprocessing_auto_threshold(prediction_view):
+    # ACT
+    prediction_view.model_input_widget.mid_input_box.setCurrentIndex(4)
+
+    # ASSERT
+    assert (
+        prediction_view._prediction_model.get_postprocessing_auto_threshold()
+        == "minimum"
+    )
+
+    # ACT
+    prediction_view.model_input_widget.mid_input_box.setCurrentIndex(6)
+
+    # ASSERT
+    assert (
+        prediction_view._prediction_model.get_postprocessing_auto_threshold()
+        == "niblack"
+    )
