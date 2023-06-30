ed2a68eee7f4fb526ea8f3f526c617bb1306caff
William Lee <williamlee0416@gmail.com>
2023-06-30 16:01:55 -0700

    Simplify event names

--- src/allencell_ml_segmenter/core/event.py
+++ src/allencell_ml_segmenter/core/event.py
@@ -6,7 +6,7 @@ class Event(Enum):
     Different application Events
     """
 
-    # Process events.  This signals long running process is active.  Porgress updates should be shown to the user.
+    # Process events.  This signals long running process is active.  Progress updates should be shown to the user.
     PROCESS_TRAINING = "training"
     PROCESS_TRAINING_PROGRESS = "training_progress"
     PROCESS_TRAINING_SHOW_ERROR = "training_error"
@@ -16,21 +16,17 @@ class Event(Enum):
     # Action events.  This signals a change in the UI.  These are a direct result of a user action
     ACTION_CHANGE_VIEW = "change_view"
     ACTION_START_TRAINING = "start_training"
-    ACTION_PREDICTION_MODEL_FILE_SELECTED = "model_file_selected"
-    ACTION_PREDICTION_PREPROCESSING_METHOD_SELECTED = (
-        "preprocessing_method_selected"
-    )
-    ACTION_PREDICTION_POSTPROCESSING_METHOD_SELECTED = (
-        "postprocessing_method_selected"
-    )
+    ACTION_PREDICTION_MODEL_FILE = "model_file"
+    ACTION_PREDICTION_PREPROCESSING_METHOD = "preprocessing_method"
+    ACTION_PREDICTION_POSTPROCESSING_METHOD = "postprocessing_method"
     ACTION_PREDICTION_POSTPROCESSING_SIMPLE_THRESHOLD_MOVED = (
         "postprocessing_simple_threshold_moved"
     )
     ACTION_PREDICTION_POSTPROCESSING_SIMPLE_THRESHOLD_TYPED = (
         "postprocessing_simple_threshold_typed"
     )
-    ACTION_PREDICTION_POSTPROCESSING_AUTO_THRESHOLD_SELECTED = (
-        "postprocessing_auto_threshold_selected"
+    ACTION_PREDICTION_POSTPROCESSING_AUTO_THRESHOLD = (
+        "postprocessing_auto_threshold"
     )
 
     # View selection events.  These can stem from a user action, or from a process (ie prediction process ends, and a new view is shown automatically).
