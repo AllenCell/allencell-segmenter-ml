8ae85b5bebfaa3868f93ccdf3f6e8c2abe76c74a
yrkim98 <yrkim98@uw.edu>
2023-07-11 11:18:31 -0700

    add tests for label_with_hint_widget

---
+++ src/allencell_ml_segmenter/_tests/widgets/test_slider_with_labels_widget.py
@@ -0,0 +1,31 @@
+import pytest
+from unittest.mock import Mock
+from allencell_ml_segmenter.prediction.model import PredictionModel
+from allencell_ml_segmenter.widgets.slider_with_labels_widget import (
+    SliderWithLabels,
+)
+from PyQt5.QtTest import QTest
+
+
+
+@pytest.fixture
+def prediction_model():
+    return PredictionModel()
+
+@pytest.fixture
+def slider_with_labels(prediction_model, qtbot):
+    return SliderWithLabels(0, 100, 1, prediction_model)
+
+def test_label_update_model(slider_with_labels):
+    # ACT
+    QTest.keyClicks(slider_with_labels._label, "3")
+
+    # Assert
+    assert slider_with_labels._model.get_postprocessing_simple_threshold() == 3.0
+
+    # ACT
+    QTest.keyClicks(slider_with_labels._label, "30")
+
+    # Assert
+    assert slider_with_labels._model.get_postprocessing_simple_threshold() == 30
+
