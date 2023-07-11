08dd7624ed503c2e9afa510f42b6a8b0a2da40a0
William Lee <williamlee0416@gmail.com>
2023-07-11 14:22:07 -0700

    Lint

--- src/allencell_ml_segmenter/_tests/widgets/test_slider_with_labels_widget.py
+++ src/allencell_ml_segmenter/_tests/widgets/test_slider_with_labels_widget.py
@@ -6,25 +6,31 @@ from allencell_ml_segmenter.widgets.slider_with_labels_widget import (
 )
 from PyQt5.QtTest import QTest
 
-#TODO redo tests once replaced with magicgui floatslider
+
+# TODO redo tests once replaced with magicgui floatslider
 @pytest.fixture
 def prediction_model():
     return PredictionModel()
 
+
 @pytest.fixture
 def slider_with_labels(prediction_model, qtbot):
     return SliderWithLabels(0, 100, prediction_model)
+
 
 def test_label_update_model(slider_with_labels):
     # ACT
     QTest.keyClicks(slider_with_labels._label, "3")
 
     # Assert
-    assert slider_with_labels._model.get_postprocessing_simple_threshold() == 3.0
+    assert (
+        slider_with_labels._model.get_postprocessing_simple_threshold() == 3.0
+    )
 
     # ACT
     QTest.keyClicks(slider_with_labels._label, "30")
 
     # Assert
-    assert slider_with_labels._model.get_postprocessing_simple_threshold() == 30
-
+    assert (
+        slider_with_labels._model.get_postprocessing_simple_threshold() == 30
+    )
