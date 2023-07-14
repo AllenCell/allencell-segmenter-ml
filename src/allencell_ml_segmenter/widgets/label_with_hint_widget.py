01aa54707173f885263a8b4e520d32af9dd588d2
William Lee <williamlee0416@gmail.com>
2023-07-14 10:51:54 -0700

    Remove unreferenced code

--- src/allencell_ml_segmenter/widgets/label_with_hint_widget.py
+++ src/allencell_ml_segmenter/widgets/label_with_hint_widget.py
@@ -20,7 +20,6 @@ class LabelWithHint(QWidget):
         self.layout().setSpacing(0)
 
         self._label: QLabel = QLabel("")
-        self._label.setStyleSheet("margin-left: 8px")
         self._label.setText(label_text)
         self.layout().addWidget(self._label, alignment=Qt.AlignLeft)
 
@@ -46,9 +45,3 @@ class LabelWithHint(QWidget):
         Sets the tooltip to be displayed when the question icon is hovered over.
         """
         self._question_mark.setToolTip(hint)
-
-    def align_left(self) -> None:
-        """
-        Removes the automatically included left margin.
-        """
-        self._label.setStyleSheet("margin-left: 0px")
