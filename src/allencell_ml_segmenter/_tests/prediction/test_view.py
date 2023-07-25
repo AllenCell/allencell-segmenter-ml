06efb60504c746aaca1c9b3ec17d603e5c137820
William Lee <williamlee0416@gmail.com>
2023-07-25 09:56:37 -0700

    Create and remove tests, update yaml

--- src/allencell_ml_segmenter/_tests/prediction/test_view.py
+++ src/allencell_ml_segmenter/_tests/prediction/test_view.py
@@ -1,23 +1,17 @@
 import pytest
 
 from allencell_ml_segmenter.core.event import Event
-from allencell_ml_segmenter.main.main_model import MainModel
 from allencell_ml_segmenter.prediction.view import PredictionView
 
 
 @pytest.fixture
-def main_model():
-    return MainModel()
+def prediction_view(qtbot):
+    return PredictionView()
 
 
-@pytest.fixture
-def prediction_view(main_model, qtbot):
-    return PredictionView(main_model)
-
-
-def test_prediction_view(prediction_view, main_model):
-    # ACT
-    main_model.dispatch(Event.VIEW_SELECTION_PREDICTION)
-
-    # ASSERT
-    assert main_model.get_current_view() == prediction_view
+# def test_prediction_view(prediction_view):
+#     # ACT
+#     main_model.dispatch(Event.VIEW_SELECTION_PREDICTION)
+#
+#     # ASSERT
+#     assert main_model.get_current_view() == prediction_view
