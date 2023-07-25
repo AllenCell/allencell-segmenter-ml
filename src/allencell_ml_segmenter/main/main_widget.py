0646b865ef30c0e2385ac502781ffc3aafb9b77a
William Lee <williamlee0416@gmail.com>
2023-07-25 09:56:18 -0700

    Refactor main widget and views to support tabs

--- src/allencell_ml_segmenter/main/main_widget.py
+++ src/allencell_ml_segmenter/main/main_widget.py
@@ -1,33 +1,17 @@
 import napari
+from PyQt5.QtWidgets import QTabWidget
 from qtpy.QtWidgets import (
     QVBoxLayout,
     QSizePolicy,
-    QStackedWidget,
 )
 
-from allencell_ml_segmenter.core.subscriber import Subscriber
-from allencell_ml_segmenter.core.event import Event
-from allencell_ml_segmenter.main.main_model import MainModel
 from allencell_ml_segmenter.prediction.view import PredictionView
-from allencell_ml_segmenter.core.view import View
 from allencell_ml_segmenter.sample.sample_view import SampleView
-from allencell_ml_segmenter.widgets.selection_widget import SelectionWidget
 
 
-class MainMeta(type(QStackedWidget), type(Subscriber)):
+class MainTabWidget(QTabWidget):
     """
-    Metaclass for MainWidget
-
-    """
-
-    pass
-
-
-class MainWidget(QStackedWidget, Subscriber, metaclass=MainMeta):
-    """
-    Main widget that is displayed in the plugin window. This widget is an empty Qwidget that supports adding and removing different
-    views from the main layout by responding to MainEvents.
-
+    Adopted and redesigned copy of MainWidget.
     """
 
     def __init__(self, viewer: napari.Viewer):
@@ -39,44 +23,6 @@ class MainWidget(QStackedWidget, Subscriber, metaclass=MainMeta):
         self.setLayout(QVBoxLayout())
         self.layout().setContentsMargins(0, 0, 0, 0)
 
-        # Model
-        self.model: MainModel = MainModel()
-        self.model.subscribe(Event.ACTION_CHANGE_VIEW, self, self.handle_event)
-
-        # Dictionaries of views to index values
-        self.view_to_index = dict()
-
-        # add sample page
-        sample_view = SampleView(self.model)
-        self.initialize_view(sample_view)
-
-        # add selection page
-        selection_view = SelectionWidget(self.model)
-        self.initialize_view(selection_view)
-
-        # add prediction page
-        prediction_view = PredictionView(self.model)
-        self.initialize_view(prediction_view)
-
-        # start on selection views
-        self.model.set_current_view(selection_view)
-
-    def handle_event(self, event: Event) -> None:
-        """
-        Handle event function for the main widget, which handles MainEvents.
-
-        inputs:
-            event - MainEvent
-        """
-        self.set_view(self.model.get_current_view())
-
-    def set_view(self, view: View) -> None:
-        """
-        Set the current views, must be initialized first
-        """
-        self.setCurrentIndex(self.view_to_index[view])
-
-    def initialize_view(self, view: View) -> None:
-        # QStackedWidget count method keeps track of how many child widgets have been added
-        self.view_to_index[view] = self.count()
-        self.addWidget(view)
+        # initialize the tabs
+        self.addTab(PredictionView(), "Prediction")
+        self.addTab(SampleView(), "Training")
