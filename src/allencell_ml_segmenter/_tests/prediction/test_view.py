2ec65a99a88652971ac552d3bded1ea2b3224f17
William Lee <williamlee0416@gmail.com>
2023-07-26 13:45:28 -0700

    Write & edit corresponding tests

--- src/allencell_ml_segmenter/_tests/prediction/test_view.py
+++ src/allencell_ml_segmenter/_tests/prediction/test_view.py
@@ -1,17 +1,23 @@
 import pytest
 
 from allencell_ml_segmenter.core.event import Event
+from allencell_ml_segmenter.main.main_model import MainModel
 from allencell_ml_segmenter.prediction.view import PredictionView
 
 
 @pytest.fixture
-def prediction_view(qtbot):
-    return PredictionView()
+def main_model():
+    return MainModel()
 
 
-# def test_prediction_view(prediction_view):
-#     # ACT
-#     main_model.dispatch(Event.VIEW_SELECTION_PREDICTION)
-#
-#     # ASSERT
-#     assert main_model.get_current_view() == prediction_view
+@pytest.fixture
+def prediction_view(main_model, qtbot):
+    return PredictionView(main_model)
+
+
+def test_prediction_view(prediction_view, main_model):
+    # ACT
+    main_model.dispatch(Event.PROCESS_TRAINING_COMPLETE)
+
+    # ASSERT
+    assert main_model.get_current_view() == prediction_view
