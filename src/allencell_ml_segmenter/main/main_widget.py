b9bb1d9ca0b462a69a0d7a8bd0e9e184d2e8a15a
William Lee <williamlee0416@gmail.com>
2023-07-26 13:44:47 -0700

    Reintroduce main model and event-based view toggling

--- src/allencell_ml_segmenter/main/main_widget.py
+++ src/allencell_ml_segmenter/main/main_widget.py
@@ -5,11 +5,23 @@ from qtpy.QtWidgets import (
     QSizePolicy,
 )
 
+from allencell_ml_segmenter.core.event import Event
+from allencell_ml_segmenter.core.view import View
+from allencell_ml_segmenter.main.main_model import MainModel
 from allencell_ml_segmenter.prediction.view import PredictionView
 from allencell_ml_segmenter.sample.sample_view import SampleView
+from allencell_ml_segmenter.core.subscriber import Subscriber
 
 
-class MainTabWidget(QTabWidget):
+class MainMeta(type(QTabWidget), type(Subscriber)):
+    """
+    Metaclass for MainTabWidget
+    """
+
+    pass
+
+
+class MainTabWidget(QTabWidget, Subscriber, metaclass=MainMeta):
     """
     Adopted and redesigned copy of MainWidget.
     """
@@ -23,6 +35,36 @@ class MainTabWidget(QTabWidget):
         self.setLayout(QVBoxLayout())
         self.layout().setContentsMargins(0, 0, 0, 0)
 
+        # main model
+        self.model: MainModel = MainModel()
+        self.model.subscribe(Event.ACTION_CHANGE_VIEW, self, self.handle_event)
+
+        # keep track of views
+        self.view_to_index = dict()
+
         # initialize the tabs
-        self.addTab(PredictionView(), "Prediction")
-        self.addTab(SampleView(), "Training")
+        prediction_view = PredictionView(self.model)
+        self.initialize_view(prediction_view, "Prediction")
+
+        training_view = SampleView(self.model)
+        self.initialize_view(training_view, "Training")
+
+    def handle_event(self, event: Event) -> None:
+        """
+        Handle event function for the main widget, which handles MainEvents.
+
+        inputs:
+            event - MainEvent
+        """
+        self.set_view(self.model.get_current_view())
+
+    def set_view(self, view: View) -> None:
+        """
+        Set the current views, must be initialized first
+        """
+        self.setCurrentIndex(self.view_to_index[view])
+
+    def initialize_view(self, view: View, title: str) -> None:
+        # QTabWidget count method keeps track of how many child widgets have been added
+        self.view_to_index[view] = self.count()
+        self.addTab(view, title)
