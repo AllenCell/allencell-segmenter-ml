ae0d751ba2d47faa692300eabdfb3a22d920fcd2
William Lee <williamlee0416@gmail.com>
2023-07-27 12:36:52 -0700

    Display training view skeleton

---
+++ src/allencell_ml_segmenter/training/view.py
@@ -0,0 +1,36 @@
+from PyQt5.QtWidgets import QVBoxLayout, QSizePolicy
+
+from allencell_ml_segmenter.core.event import Event
+from allencell_ml_segmenter.core.subscriber import Subscriber
+from allencell_ml_segmenter.core.view import View
+from allencell_ml_segmenter.main.main_model import MainModel
+from allencell_ml_segmenter.training.image_selection_widget import (
+    ImageSelectionWidget,
+)
+
+
+class TrainingView(View, Subscriber):
+    """
+    Holds widgets pertinent to training processes.
+    """
+
+    def __init__(self, main_model: MainModel):
+        super().__init__()
+
+        self._main_model: MainModel = main_model
+        # self._training_model: TrainingModel = TrainingModel()
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+        self.layout().setContentsMargins(0, 0, 0, 0)
+        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        self.setLayout(QVBoxLayout())
+
+        self.layout().addWidget(ImageSelectionWidget())
+
+        self._main_model.subscribe(
+            Event.VIEW_SELECTION_TRAINING,
+            self,
+            lambda e: self._main_model.set_current_view(self),
+        )
