1f4a832a4bfe047aeb16fca1a079f47af994c53d
william.lee <williamlee0416@gmail.com>
2023-07-28 16:15:38 -0700

    Make test less specific

--- src/allencell_ml_segmenter/_tests/main/test_main_widget.py
+++ src/allencell_ml_segmenter/_tests/main/test_main_widget.py
@@ -1,6 +1,9 @@
+from typing import Set
+
 import pytest
 import napari
 
+from allencell_ml_segmenter.core.view import View
 from allencell_ml_segmenter.main.main_widget import MainTabWidget
 from unittest.mock import Mock
 
@@ -16,20 +19,15 @@ def main_tab_widget(qtbot):
 
 
 def test_handle_action_change_view_event(main_tab_widget):
-    # ACT: have the model dispatch the action change view event
-    main_tab_widget._model.set_current_view(main_tab_widget._training_view)
+    # ARRANGE
+    views: Set[View] = main_tab_widget._view_to_index.keys()
 
-    # ASSERT: check that the main widget's current view (after setting) is same as the model's current view
-    assert (
-        main_tab_widget.currentIndex()
-        == main_tab_widget._view_to_index[main_tab_widget._training_view]
-    )
+    for view in views:
+        # ACT: have the model dispatch the action change view event
+        main_tab_widget._model.set_current_view(view)
 
-    # ACT: have the model dispatch the action change view event, in case that the first act statement didn't do anything
-    main_tab_widget._model.set_current_view(main_tab_widget._prediction_view)
-
-    # ASSERT: check that the main widget's current view (after setting) is same as the model's current view
-    assert (
-        main_tab_widget.currentIndex()
-        == main_tab_widget._view_to_index[main_tab_widget._prediction_view]
-    )
+        # ASSERT: check that the main widget's current view (after setting) is same as the model's current view
+        assert (
+            main_tab_widget.currentIndex()
+            == main_tab_widget._view_to_index[view]
+        )
