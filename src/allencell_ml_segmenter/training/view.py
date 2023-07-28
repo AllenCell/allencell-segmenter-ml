ea7b2f1bc1cf6d23025924deb449a728a888c154
william.lee <williamlee0416@gmail.com>
2023-07-28 14:18:27 -0700

    Tweak view alignment

--- src/allencell_ml_segmenter/training/view.py
+++ src/allencell_ml_segmenter/training/view.py
@@ -1,6 +1,13 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QVBoxLayout, QSizePolicy, QLabel, QPushButton
+from PyQt5.QtWidgets import (
+    QVBoxLayout,
+    QSizePolicy,
+    QLabel,
+    QPushButton,
+    QFrame,
+)
 
+from allencell_ml_segmenter._style import Style
 from allencell_ml_segmenter.core.event import Event
 from allencell_ml_segmenter.core.subscriber import Subscriber
 from allencell_ml_segmenter.core.view import View
@@ -26,23 +33,36 @@ class TrainingView(View, Subscriber):
 
         self.setLayout(QVBoxLayout())
         self.layout().setContentsMargins(0, 0, 0, 0)
+        self.layout().setSpacing(0)
         self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
 
         self._title: QLabel = QLabel("SEGMENTATION MODEL TRAINING", self)
         self._title.setObjectName("title")
-        self.layout().addWidget(self._title, alignment=Qt.AlignCenter)
+        self.layout().addWidget(
+            self._title, alignment=Qt.AlignHCenter | Qt.AlignBottom
+        )
 
         image_selection_widget: ImageSelectionWidget = ImageSelectionWidget()
         image_selection_widget.setObjectName("imageSelection")
-        self.layout().addWidget(image_selection_widget)
 
         model_selection_widget: ModelSelectionWidget = ModelSelectionWidget()
         model_selection_widget.setObjectName("modelSelection")
-        self.layout().addWidget(model_selection_widget)
+
+        # Dummy divs allow for easy alignment
+        top_container, top_dummy = QVBoxLayout(), QFrame()
+        bottom_container, bottom_dummy = QVBoxLayout(), QFrame()
+
+        top_container.addWidget(image_selection_widget)
+        top_dummy.setLayout(top_container)
+        self.layout().addWidget(top_dummy, alignment=Qt.AlignTop)
+
+        bottom_container.addWidget(model_selection_widget)
+        bottom_dummy.setLayout(bottom_container)
+        self.layout().addWidget(bottom_dummy, alignment=Qt.AlignTop)
 
         self._train_btn: QPushButton = QPushButton("Start training")
         self._train_btn.setObjectName("trainBtn")
-        self.layout().addWidget(self._train_btn)
+        self.layout().addWidget(self._train_btn, alignment=Qt.AlignTop)
 
         self._main_model.subscribe(
             Event.VIEW_SELECTION_TRAINING,
@@ -51,4 +71,4 @@ class TrainingView(View, Subscriber):
         )
 
         # apply styling
-        self.setStyleSheet("training_view.qss")
+        self.setStyleSheet(Style.get_stylesheet("training_view.qss"))
