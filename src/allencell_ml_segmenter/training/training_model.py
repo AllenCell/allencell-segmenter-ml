b05e032e0181963eb5d7661794419ca4a36c4630
yrkim98 <yrkim98@uw.edu>
2023-08-03 11:30:53 -0700

    lint

--- src/allencell_ml_segmenter/training/training_model.py
+++ src/allencell_ml_segmenter/training/training_model.py
@@ -29,6 +29,7 @@ class PatchSize(Enum):
     Patch size for training, and their respective patch shapes.
     TODO: get from benji
     """
+
     SMALL = [1, 3, 3]
     MEDIUM = [16, 32, 32]
     LARGE = [20, 40, 40]
