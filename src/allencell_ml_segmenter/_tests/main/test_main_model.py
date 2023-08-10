fdc5a5d60ebc53ebda55437802aeef62647b9d29
william.lee <williamlee0416@gmail.com>
2023-08-10 14:29:42 -0700

    Move main model to main

--- src/allencell_ml_segmenter/_tests/main/test_main_model.py
+++ src/allencell_ml_segmenter/_tests/main/test_main_model.py
@@ -8,29 +8,44 @@ from allencell_ml_segmenter._tests.fakes.fake_subscriber import FakeSubscriber
 
 @pytest.fixture
 def main_model() -> MainModel:
+    """
+    Fixture for MainModel testing.
+    """
     return MainModel()
 
 
 @pytest.fixture
 def fake_subscriber() -> FakeSubscriber:
+    """
+    Fixture for FakeSubscriber testing.
+    """
     return FakeSubscriber()
 
 
 def test_get_current_view(main_model: MainModel) -> None:
+    """
+    Tests that the current view is correctly retrievable
+    """
+    # ASSERT
     assert main_model.get_current_view() is None
+
+    # ARRANGE
     mock_view: View = Mock(spec=View)
     main_model._current_view = mock_view
 
+    # ACT/ASSERT
     assert main_model.get_current_view() == mock_view
 
 
 def test_set_current_view(
     main_model: MainModel, fake_subscriber: FakeSubscriber
 ) -> None:
-    # set a mock views
+    """
+    Tests that the current view is correctly settable.
+    """
+    # ARRANGE
     mock_view: View = Mock(spec=View)
 
-    # ARRANGE
     main_model.subscribe(
         Event.ACTION_CHANGE_VIEW, fake_subscriber, fake_subscriber.handle
     )
