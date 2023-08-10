72f5e16bc17813409ee343ecbcf3c693f36cabb9
william.lee <williamlee0416@gmail.com>
2023-08-10 11:51:51 -0700

    LabelWithHint documentation

--- src/allencell_ml_segmenter/_tests/widgets/test_label_with_hint_widget.py
+++ src/allencell_ml_segmenter/_tests/widgets/test_label_with_hint_widget.py
@@ -12,27 +12,42 @@ def label_with_hint(qtbot: QtBot) -> LabelWithHint:
 def test_set_label_text_updates_label_text(
     label_with_hint: LabelWithHint,
 ) -> None:
-    # Act
-    label_with_hint.set_label_text("Hello")
+    """
+    Tests that the label text is updated when set_label_text is called.
+    """
+    # ARRANGE
+    label_txt: str = "Text goes here"
 
-    # Assert
-    assert label_with_hint._label.text() == "Hello"
+    # ACT
+    label_with_hint.set_label_text(label_txt)
+
+    # ASSERT
+    assert label_with_hint._label.text() == label_txt
 
 
 def test_set_hint_updates_question_mark_tooltip(
     label_with_hint: LabelWithHint,
 ) -> None:
-    # Act
-    label_with_hint.set_hint("This is a hint")
+    """
+    Tests that the question mark tooltip is updated when set_hint is called.
+    """
+    # ARRANGE
+    tooltip: str = "Helpful hint"
 
-    # Assert
-    assert label_with_hint._question_mark.toolTip() == "This is a hint"
+    # ACT
+    label_with_hint.set_hint(tooltip)
+
+    # ASSERT
+    assert label_with_hint._question_mark.toolTip() == tooltip
 
 
 def test_layout_contains_label_and_question_mark(
     label_with_hint: LabelWithHint,
 ) -> None:
-    # Act/Assert
+    """
+    Tests that the widget layout contains the both label and question mark.
+    """
+    # ASSERT
     assert label_with_hint.layout().indexOf(label_with_hint._label) != -1
     assert (
         label_with_hint.layout().indexOf(label_with_hint._question_mark) != -1
