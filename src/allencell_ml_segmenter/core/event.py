c278d9c5c9c36a248e22b95831c13751e73a2d0d
william.lee <williamlee0416@gmail.com>
2023-08-10 14:46:22 -0700

    Event

--- src/allencell_ml_segmenter/core/event.py
+++ src/allencell_ml_segmenter/core/event.py
@@ -6,7 +6,7 @@ class Event(Enum):
     Different application Events
     """
 
-    # Process events.  This signals long running process is active.  Progress updates should be shown to the user.
+    # Process events. This signals that a long-running process is active. Progress updates should be shown to the user.
     PROCESS_TRAINING = "training"
     PROCESS_TRAINING_PROGRESS = "training_progress"
     PROCESS_TRAINING_SHOW_ERROR = "training_error"
@@ -14,7 +14,7 @@ class Event(Enum):
     PROCESS_TRAINING_COMPLETE = "training_complete"
     PROCESS_PREDICTION = "prediction"
 
-    # Action events.  This signals a change in the UI.  These are a direct result of a user action
+    # Action events. This signals a change in the UI. These are a direct result of a user action
     ACTION_CHANGE_VIEW = "change_view"
     ACTION_START_TRAINING = "start_training"
     ACTION_PREDICTION_MODEL_FILE = "model_file"
@@ -27,7 +27,7 @@ class Event(Enum):
         "postprocessing_auto_threshold"
     )
 
-    # View selection events.  These can stem from a user action, or from a process (ie prediction process ends, and a new view is shown automatically).
+    # View selection events. These can stem from a user action, or from a process (i.e. prediction process ends, and a new view is shown automatically).
     VIEW_SELECTION_TRAINING = "training_selected"
     VIEW_SELECTION_PREDICTION = "prediction_selected"
     VIEW_SELECTION_MAIN = "main_selected"
