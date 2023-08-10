ecb4a8487f4d3d05c3ea1952c0bce6b9cdd731ce
william.lee <williamlee0416@gmail.com>
2023-08-10 11:36:09 -0700

    Documentation for styles

--- src/allencell_ml_segmenter/_style.py
+++ src/allencell_ml_segmenter/_style.py
@@ -5,10 +5,16 @@ from allencell_ml_segmenter.core.directories import Directories
 
 
 class Style:
+    """
+    Helper class that enables the retrieval of custom Qt stylesheets. Implementation adapted from Segmenter Classic.
+    """
     cache: Dict[str, str] = dict()
 
     @classmethod
     def get_stylesheet(cls, name: str) -> str:
+        """
+        Retrieve a stylesheet from the style directory. Stylesheets are cached in memory for efficiency.
+        """
         if name is None:
             raise ValueError("Stylesheet name can't be None")
         if not name.endswith(".qss"):
@@ -21,6 +27,9 @@ class Style:
 
     @classmethod
     def _load_from_file(cls, name: str) -> str:
+        """
+        Helper method that loads a stylesheet that has not yet been cached.
+        """
         path: Path = Directories.get_style_dir() / name
         with open(path, "r") as handle:
             return handle.read()
