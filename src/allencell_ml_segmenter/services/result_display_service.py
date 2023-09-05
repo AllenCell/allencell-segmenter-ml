f393b3392c3b8fa6b03bb3567c112aef81a2efa7
chrishu <chrishu@alleninstitute.org>
2023-09-05 12:28:27 -0400

    formatting

--- src/allencell_ml_segmenter/services/result_display_service.py
+++ src/allencell_ml_segmenter/services/result_display_service.py
@@ -20,16 +20,19 @@ class ResultDisplayService(Subscriber):
             self._model.subscribe(
                 Event.PROCESS_PREDICTION_COMPLETE,
                 self,
-                lambda x: self.grab_and_display_results(self._model.get_output_directory()),
+                lambda x: self.grab_and_display_results(
+                    self._model.get_output_directory()
+                ),
             )
         elif isinstance(model, TrainingModel):
             self._model.subscribe(
                 Event.PROCESS_TRAINING_COMPLETE,
                 self,
                 # TODO change this to grab the correct directory
-                lambda x: self.grab_and_display_results(self._model.get_output_directory()),
+                lambda x: self.grab_and_display_results(
+                    self._model.get_output_directory()
+                ),
             )
-
 
     def grab_and_display_results(self, dir_to_grab: Path):
         output_dir: Path = dir_to_grab
@@ -41,13 +44,17 @@ class ResultDisplayService(Subscriber):
             for idx, file in enumerate(files):
                 try:
                     image = AICSImage(str(file), reader=TiffReader)
-                    self.add_image_to_viewer(image.data, f"Segmentation {str(idx)}")
+                    self.add_image_to_viewer(
+                        image.data, f"Segmentation {str(idx)}"
+                    )
                 except Exception as e:
                     print(e)
-                    print(f"Could not load image {str(file)} into napari viewer. Image cannot be opened by AICSImage")
+                    print(
+                        f"Could not load image {str(file)} into napari viewer. Image cannot be opened by AICSImage"
+                    )
 
     def grab_files_from_folder(self, path: Path):
-        allfiles = path.glob('**/*')
+        allfiles = path.glob("**/*")
         return [x for x in allfiles if x.is_file()]
 
     def add_image_to_viewer(self, image: AICSImage, display_name: str):
