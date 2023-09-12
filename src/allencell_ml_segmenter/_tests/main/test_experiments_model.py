faf90873919b59c70d9c53cbd8072c6298f3d48d
chrishu <chrishu@alleninstitute.org>
2023-09-12 13:29:21 -0400

    experiment model testing

--- src/allencell_ml_segmenter/_tests/main/test_experiments_model.py
+++ src/allencell_ml_segmenter/_tests/main/test_experiments_model.py
@@ -1,8 +1,9 @@
 # from pathlib import Path
+from pathlib import PurePath
 import pytest
 from allencell_ml_segmenter.config.cyto_dl_config import CytoDlConfig
 
-# from allencell_ml_segmenter.main.experiments_model import ExperimentsModel
+from allencell_ml_segmenter.main.experiments_model import ExperimentsModel
 
 
 @pytest.fixture
@@ -13,7 +14,12 @@ def config() -> CytoDlConfig:
     return CytoDlConfig()
 
 
-# def test_refresh_experiments(self) -> None:
-#     model = ExperimentsModel(CytoDlConfig(Path(), Path()))
-#     experimentsModel = model.refresh_experiments()
-#     assert experimentsModel is not None
+def test_refresh_experiments() -> None:
+    model = ExperimentsModel(CytoDlConfig(
+                    cyto_dl_home_path=PurePath(__file__).parent
+                    / "cyto_dl_home",
+                    user_experiments_path=PurePath(__file__).parent
+                    / "experiments_home",
+                ))
+    experimentsModel = model.refresh_experiments()
+    assert experimentsModel is not None
