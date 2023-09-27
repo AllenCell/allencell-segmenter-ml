bcccfb9a3a22c90839242e3a140aa91840513e96
yrkim98 <yrkim98@uw.edu>
2023-09-27 18:30:23 +0200

    model done

--- src/allencell_ml_segmenter/curation/curation_model.py
+++ src/allencell_ml_segmenter/curation/curation_model.py
@@ -12,7 +12,26 @@ class CurationModel(Publisher):
 
     def __init__(self):
         super().__init__()
-        self._raw_image_directory = None
+        self._raw_image_directory: Path = None
+        self._seg1_directory: Path = None
+        self._seg2_directory: Path = None
 
-    def set_raw_directory(self, dir):
+    def set_raw_directory(self, dir: Path):
         self._raw_image_directory = dir
+
+    def get_raw_directory(self) -> Path:
+        return self._raw_image_directory
+
+    def set_seg1_directory(self, dir: Path):
+        self._seg1_directory = dir
+
+    def get_seg1_directory(self) -> Path:
+        return self._seg1_directory
+
+    def set_seg2_directory(self, dir: Path):
+        self._seg2_directory = dir
+
+    def get_seg2_directory(self) -> Path:
+        return self._seg2_directory
+
+
