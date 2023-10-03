46853ffa5a5271872f312d1a32ab197be117c8d2
chrishu <chrishu@alleninstitute.org>
2023-10-03 16:41:52 -0400

    updating broken tests

--- src/allencell_ml_segmenter/_tests/training/test_model_selection_widget.py
+++ src/allencell_ml_segmenter/_tests/training/test_model_selection_widget.py
@@ -33,14 +33,11 @@ def training_model() -> TrainingModel:
 @pytest.fixture
 def model_selection_widget(
     experiment_model: IExperimentsModel,
-    training_model: TrainingModel,
 ) -> ModelSelectionWidget:
     """
     Fixture that creates an instance of ModelSelectionWidget for testing.
     """
-    return ModelSelectionWidget(
-        training_model=training_model, experiments_model=experiment_model
-    )
+    return ModelSelectionWidget(experiments_model=experiment_model)
 
 
 def test_radio_new_slot(
@@ -78,40 +75,26 @@ def test_radio_existing_slot(
     # ASSERT
     assert model_selection_widget._combo_box_existing_models.isEnabled()
 
-
-def test_checkbox_slot(
-    qtbot: QtBot, model_selection_widget: ModelSelectionWidget
+def test_select_new_model_radio(
+    qtbot: QtBot,
+    model_selection_widget: ModelSelectionWidget,
+    experiment_model: IExperimentsModel,
 ) -> None:
-    """
-    Test the slot connected to the timeout checkbox.
-    """
-    # ASSERT (QLineEdit related to timeout limit is disabled by default)
-    assert not model_selection_widget._max_time_in_hours_input.isEnabled()
+    # ARRANGE
+    experiment_model.set_checkpoint("dummy_checkpoint")
+    experiment_model.set_experiment_name("dummy_experiment")
 
-    # ACT (enable QLineEdit related to timeout limit)
-    with qtbot.waitSignal(
-        model_selection_widget._max_time_checkbox.stateChanged
-    ):
-        model_selection_widget._max_time_checkbox.click()
+    # ACT - press "start a new model" radio button, which should set model_path to None
+    with qtbot.waitSignal(model_selection_widget._radio_new_model.toggled):
+        model_selection_widget._radio_new_model.click()  # enables the combo box
 
     # ASSERT
-    assert model_selection_widget._max_time_in_hours_input.isEnabled()
-
-    # ACT (disabled QLineEdit related to timeout limit)
-    with qtbot.waitSignal(
-        model_selection_widget._max_time_checkbox.stateChanged
-    ):
-        model_selection_widget._max_time_checkbox.click()
-
-    # ASSERT
-    assert not model_selection_widget._max_time_in_hours_input.isEnabled()
-
+    assert experiment_model.get_checkpoint() is None
 
 def test_select_existing_model_option(
     qtbot: QtBot,
     experiment_model: IExperimentsModel,
     model_selection_widget: ModelSelectionWidget,
-    training_model: TrainingModel,
 ) -> None:
     """
     Tests that the slots connected to the "start a new model" radio button and the existing model QCombBox properly set the model path field.
@@ -127,96 +110,7 @@ def test_select_existing_model_option(
         # ACT
         # Invariant: options in existing_models combo were added in the order the appear in the model.
         model_selection_widget._combo_box_existing_models.setCurrentIndex(i)
-        training_model.set_checkpoint(dummy_checkpoint)
+        experiment_model.set_checkpoint(dummy_checkpoint)
 
         # ASSERT
-        assert experiment == training_model.get_experiment_name()
-
-
-def test_select_new_model_radio(
-    qtbot: QtBot,
-    model_selection_widget: ModelSelectionWidget,
-    training_model: TrainingModel,
-) -> None:
-    # ARRANGE
-    training_model.set_checkpoint("dummy_checkpoint")
-    training_model.set_experiment_name("dummy_experiment")
-
-    # ACT - press "start a new model" radio button, which should set model_path to None
-    with qtbot.waitSignal(model_selection_widget._radio_new_model.toggled):
-        model_selection_widget._radio_new_model.click()  # enables the combo box
-
-    # ASSERT
-    assert training_model.get_checkpoint() is None
-
-
-def test_set_patch_size(
-    model_selection_widget: ModelSelectionWidget,
-    training_model: TrainingModel,
-) -> None:
-    """
-    Tests that using the associated combo box properly sets the patch size field.
-    """
-
-    for index, patch in enumerate(PatchSize):
-        # ACT
-        model_selection_widget._patch_size_combo_box.setCurrentIndex(index)
-
-        # ASSERT
-        assert training_model.get_patch_size() == patch
-
-
-def test_set_image_dimensions(
-    qtbot: QtBot,
-    model_selection_widget: ModelSelectionWidget,
-    training_model: TrainingModel,
-) -> None:
-    """
-    Tests that checking the associated radio buttons properly sets the image dimensions.
-    """
-    # ACT
-    with qtbot.waitSignal(model_selection_widget._radio_2d.toggled):
-        model_selection_widget._radio_2d.click()
-
-    # ASSERT
-    assert training_model.get_image_dims() == 2
-
-    # ACT
-    with qtbot.waitSignal(model_selection_widget._radio_3d.toggled):
-        model_selection_widget._radio_3d.click()
-
-    # ASSERT
-    assert training_model.get_image_dims() == 3
-
-
-def test_set_max_epoch(
-    qtbot: QtBot,
-    model_selection_widget: ModelSelectionWidget,
-    training_model: TrainingModel,
-) -> None:
-    """
-    Tests that the max epoch field is properly set by the associated QLineEdit.
-    """
-    # ACT
-    qtbot.keyClicks(model_selection_widget._max_epoch_input, "100")
-
-    # ASSERT
-    assert training_model.get_max_epoch() == 100
-
-
-def test_set_max_time(
-    qtbot: QtBot,
-    model_selection_widget: ModelSelectionWidget,
-    training_model: TrainingModel,
-) -> None:
-    """
-    Tests that the max time field is properly set by the associated QLineEdit.
-    """
-    # ACT
-    with qtbot.waitSignal(model_selection_widget._max_time_checkbox.toggled):
-        model_selection_widget._max_time_checkbox.click()  # enables the QLineEdit
-
-    qtbot.keyClicks(model_selection_widget._max_time_in_hours_input, "1")
-
-    # ASSERT
-    assert training_model.get_max_time() == 3600
+        assert experiment == experiment_model.get_experiment_name()
