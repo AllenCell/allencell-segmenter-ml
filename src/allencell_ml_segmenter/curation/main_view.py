6e1c785e0e11e109c8c8a111e4a3e18360e81144
Brian Kim <yrkim98@uw.edu>
2023-12-01 12:32:24 -0800

    final push minor error during testing- last image will update progress bar to max + 1

--- src/allencell_ml_segmenter/curation/main_view.py
+++ src/allencell_ml_segmenter/curation/main_view.py
@@ -287,7 +287,9 @@ class CurationMainView(View):
         """
         increment the progress bar by 1
         """
-        if (self._curation_model.get_curation_index() + 1) <= len(self._curation_model.get_raw_images()):
+        if (self._curation_model.get_curation_index() + 1) <= len(
+            self._curation_model.get_raw_images()
+        ):
             # update progress bar
             self.progress_bar.setValue(self.progress_bar.value() + 1)
             # set progress bar hint
