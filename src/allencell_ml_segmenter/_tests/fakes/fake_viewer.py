d0419177ff44c7b1f0f419672224ffb04078d049
Brian Kim <yrkim98@uw.edu>
2023-12-12 09:20:43 -0800

    use fake layer and fake viewer for tests

--- src/allencell_ml_segmenter/_tests/fakes/fake_viewer.py
+++ src/allencell_ml_segmenter/_tests/fakes/fake_viewer.py
@@ -1,9 +1,25 @@
 from allencell_ml_segmenter.main.i_viewer import IViewer
 
+import napari
+from typing import List
+from napari.layers import Layer
+
+
+class FakeLayer:
+    def __init__(self):
+        self.removed: List[Layer] = []
+
+    def remove(self, layer_remove: Layer):
+        self.removed.append(layer_remove)
+
+    def is_removed(self, layer: Layer):
+        return layer in self.removed
+
 
 class FakeViewer(IViewer):
     def __init__(self):
         self._viewer = None
+        self.layers = FakeLayer()
 
     def add_image(image, name):
         pass
