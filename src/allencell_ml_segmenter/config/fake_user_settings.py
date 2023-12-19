26aa1a544a93907d3a0a145b162ee7e3062c7cd7
chrishu <chrishu@alleninstitute.org>
2023-12-19 15:02:32 -0500

    lint

--- src/allencell_ml_segmenter/config/fake_user_settings.py
+++ src/allencell_ml_segmenter/config/fake_user_settings.py
@@ -4,9 +4,14 @@ from qtpy.QtWidgets import QWidget
 
 
 class FakeUserSettings(IUserSettings):
-    def __init__(self, cyto_dl_home_path=None, user_experiments_path=None, prompt_response: Path = None):
-        self.cyto_dl_home_path=cyto_dl_home_path
-        self.user_experiments_path=user_experiments_path
+    def __init__(
+        self,
+        cyto_dl_home_path=None,
+        user_experiments_path=None,
+        prompt_response: Path = None,
+    ):
+        self.cyto_dl_home_path = cyto_dl_home_path
+        self.user_experiments_path = user_experiments_path
         self.prompt_response: Path = prompt_response
 
     def get_cyto_dl_home_path(self) -> Path:
