c5ff229f2dd56a0814edbe6d2cffd49ff1525977
Brian Kim <yrkim98@uw.edu>
2024-02-08 09:23:18 -0800

    use fakeviewer to test main widget

--- src/allencell_ml_segmenter/_tests/fakes/fake_viewer.py
+++ src/allencell_ml_segmenter/_tests/fakes/fake_viewer.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 from allencell_ml_segmenter.main.i_viewer import IViewer
 
 import napari
-from typing import List
+from typing import List, Callable
 from napari.layers import Layer
 from unittest.mock import Mock
 from napari.layers.shapes.shapes import Shapes
@@ -10,12 +12,20 @@ from napari.layers.shapes.shapes import Shapes
 class FakeLayer:
     def __init__(self):
         self.removed: List[Layer] = []
+        self.connected: List[Callable] = []
 
     def remove(self, layer_remove: Layer):
         self.removed.append(layer_remove)
 
     def is_removed(self, layer: Layer):
         return layer in self.removed
+
+    def connect(self, callable: Callable):
+        self.connected.append(callable)
+
+class FakeNapariEvent():
+    def __init__(self):
+        self.layers_change: FakeLayer = FakeLayer()
 
 
 class FakeViewer(IViewer):
@@ -26,6 +36,8 @@ class FakeViewer(IViewer):
         self.images_added = dict()
         self.shapes_layers_added = []
         self.shapes_layers_removed = []
+        self.layers_change_event = None
+        self.events = FakeNapariEvent()
 
     def add_image(self, image, name=None):
         self.images_added[name] = image
@@ -45,3 +57,9 @@ class FakeViewer(IViewer):
 
     def is_layer_removed(self, layer: Layer):
         return self.layers.is_removed(layer)
+
+    def get_paths_of_image_layers(self) -> List:
+        return list(self.images_added.keys())
+
+    def subscribe_layers_change_event(self, function):
+        self.layers_change_event = function
