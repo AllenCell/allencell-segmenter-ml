e89484ab5c276ec5cc587a0ad507ca7c31ee9b3d
Brian Kim <yrkim98@uw.edu>
2024-02-12 09:45:28 -0800

    lint

--- src/allencell_ml_segmenter/prediction/file_input_widget.py
+++ src/allencell_ml_segmenter/prediction/file_input_widget.py
@@ -121,7 +121,9 @@ class PredictionFileInput(QWidget):
 
         self._browse_dir_edit: InputButton = InputButton(
             self._model,
-            lambda dir: self._model.set_input_image_path(Path(dir), extract_channels=True),
+            lambda dir: self._model.set_input_image_path(
+                Path(dir), extract_channels=True
+            ),
             "Select directory...",
             FileInputMode.DIRECTORY_OR_CSV,
         )
