5d44286ef152851885e4aecd22cef4ab17b45c1f
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-02-16 09:40:10 -0800

    fixed some linter issues

--- src/allencell_ml_segmenter/core/progress_tracker.py
+++ src/allencell_ml_segmenter/core/progress_tracker.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
 
+
 class ProgressTracker():
     """
     Base class for all ProgressTrackers to inherit from. A ProgressTracker
@@ -7,30 +8,30 @@ class ProgressTracker():
     progress_maximum. The progress value can be used by PyQt progress bars
     for example.
     """
-    
-    def __init__(self, progress_minimum: int=0, progress_maximum: int=0):
+
+    def __init__(self, progress_minimum: int = 0, progress_maximum: int = 0):
         self._progress_minimum: int = progress_minimum
         self._progress_maximum: int = progress_maximum
         self._progress: int = progress_minimum
 
     def get_progress_minimum(self) -> int:
         return self._progress_minimum
-    
+
     def get_progress_maximum(self) -> int:
         return self._progress_maximum
-    
+
     def get_progress(self) -> int:
         return self._progress
-    
+
     def set_progress_minimum(self, progress_minimum: int) -> None:
         self._progress_minimum = progress_minimum
-    
+
     def set_progress_maximum(self, progress_maximum: int) -> None:
         self._progress_maximum = progress_maximum
-    
+
     def set_progress(self, progress: int) -> None:
         self._progress = progress if progress >= self._progress_minimum and progress <= self._progress_maximum else self._progress
-    
+
     @abstractmethod
     def start_tracker(self) -> None:
         pass
