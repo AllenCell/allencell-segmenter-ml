e7e500ff5f8808c127f9b74102fe0defa41bda49
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-02-26 16:24:13 -0800

    post bumpver testing

--- publish_bumpver_handler.py
+++ publish_bumpver_handler.py
@@ -7,13 +7,13 @@ from typing import Set, List
 def main():
     if len(sys.argv) < 2:
         raise ValueError("No component specified for bumping version")
-    
+
     component: str = sys.argv[1].lower()
     valid_options: Set[str] = {"major", "minor", "patch", "dev"}
 
     if component not in valid_options:
         raise ValueError(f"Component must be one of {valid_options}")
-    
+
     show_output: subprocess.CompletedProcess = subprocess.run(["bumpver", "show", "-n"], capture_output=True)
     if show_output.returncode != 0:
         raise RuntimeError(f"bumpver exited with code {show_output.returncode}")
@@ -34,12 +34,14 @@ def main():
             update_output = subprocess.run(["bumpver", "update", "--tag=final"])
         else:
             raise ValueError("Cannot update major or minor version while dev version is current")
+
     elif len(version_components) == 3:
         if component == "dev":
             # increment patch and begin at dev0 (e.g. 1.0.0 -> 1.0.1.dev0)
             update_output = subprocess.run(["bumpver", "update", "--patch", "--tag=dev"])
         else:
             update_output = subprocess.run(["bumpver", "update", f"--{component}"])
+
     else:
         raise ValueError(f"Unknown version format: {current_version}. Expected MAJOR.MINOR.PATCH[.PYTAGNUM]")
     
@@ -52,5 +54,38 @@ if __name__ == "__main__":
 
 """
 TESTING:
-bumpver update --set-version 1.0.0
+- add and commit any changes (keep track of this commit hash)
+- bumpver update --set-version 1.0.0
+
+- python publish_bumpver_handler.py
+  - expect: ValueError
+
+- python publish_bumpver_handler.py fake
+  - expect: ValueError
+
+- python publish_bumpver_handler.py major
+  - expect: version updated to 2.0.0
+
+- python publish_bumpver_handler.py minor
+  - expect: version updated to 2.1.0
+
+- python publish_bumpver_handler.py patch
+  - expect: version updated to 2.1.1
+
+- python publish_bumpver_handler.py dev
+  - expect: version updated to 2.1.2.dev0
+
+- python publish_bumpver_handler.py dev
+  - expect: version updated to 2.1.2.dev1
+
+- python publish_bumpver_handler.py major
+  - expect: ValueError
+
+- python publish_bumpver_handler.py minor
+  - expect: ValueError
+
+- python publish_bumpver_handler.py patch
+  - expect: version updated to 2.1.2
+
+- git reset --hard {hash of the commit made at the beginning}
 """
