08c70c61f50a26415db38b7bda1fa3442cd2653d
brian.kim <yrkim98@uw.edu>
2024-02-29 11:30:24 -0800

    prediction infra changed to support progressbar

--- src/allencell_ml_segmenter/core/event.py
+++ src/allencell_ml_segmenter/core/event.py
@@ -32,8 +32,9 @@ class Event(Enum):
     ACTION_PREDICTION_POSTPROCESSING_AUTO_THRESHOLD = (
         "postprocessing_auto_threshold"
     )
-    ACTION_PREDICTION_INITIATED = "prediction_initiated"
+    ACTION_PREDICTION_GET_IMAGE_PATHS_FROM_NAPARI = "prediction_get_image_paths_from_napari"
     ACTION_PREDICTION_INPUT_PATH_SELECTED = "prediction_input_path_selected"
+    ACTION_PREDICTION_WRITE_CSV = "prediction_write_csv"
     ACTION_PREDICTION_EXTRACT_CHANNELS = "prediction_extract_channels"
 
     # Curation
