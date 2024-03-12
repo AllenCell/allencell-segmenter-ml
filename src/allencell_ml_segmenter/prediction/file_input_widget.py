1c61ff75fec1b9da92b9ab816471d69cdb2e8fbe
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-03-12 15:48:02 -0700

    messy working version of prediction

--- src/allencell_ml_segmenter/prediction/file_input_widget.py
+++ src/allencell_ml_segmenter/prediction/file_input_widget.py
@@ -31,8 +31,7 @@ from allencell_ml_segmenter.widgets.check_box_list_widget import (
 )
 
 from allencell_ml_segmenter.main.viewer import Viewer
-
-
+from allencell_ml_segmenter.prediction.service import ModelFileService
 class PredictionFileInput(QWidget):
     """
     A widget containing file inputs for the input to a model prediction.
@@ -41,11 +40,12 @@ class PredictionFileInput(QWidget):
     TOP_TEXT: str = "On-screen image(s)"
     BOTTOM_TEXT: str = "Image(s) from a directory"
 
-    def __init__(self, model: PredictionModel, viewer: Viewer):
+    def __init__(self, model: PredictionModel, viewer: Viewer, service: ModelFileService):
         super().__init__()
 
         self._model: PredictionModel = model
         self._viewer: Viewer = viewer
+        self._service: ModelFileService = service
         self.setLayout(QVBoxLayout())
         self.layout().setContentsMargins(0, 0, 0, 0)
         self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
@@ -205,6 +205,8 @@ class PredictionFileInput(QWidget):
 
     def _update_layer_list(self, event: Optional[NapariEvent] = None) -> None:
         self._image_list.clear()
+        self._model.set_selected_paths([], extract_channels=False)
+        self._reset_channel_combobox()
         for layer in self._viewer.get_layers():
             path_of_layer_image: str = layer.source.path
             if path_of_layer_image:
@@ -233,6 +235,7 @@ class PredictionFileInput(QWidget):
                 # could have unselected the img we got channels from originally, so need to re-extract
                 # as long as there are still some images selected
                 if len(selected_indices) == 0:
+                    self._service.stop_channel_extraction() # stop so combobox doesn't reset after thread is finished
                     self._reset_channel_combobox()
 
                 self._model.set_selected_paths(
