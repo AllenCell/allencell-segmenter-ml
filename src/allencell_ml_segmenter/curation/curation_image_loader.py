743295fa0de5c4fe6dd4d538d481b2c033b89185
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-03-18 16:52:38 -0700

    added comments and typing

--- src/allencell_ml_segmenter/curation/curation_image_loader.py
+++ src/allencell_ml_segmenter/curation/curation_image_loader.py
@@ -1,11 +1,10 @@
 from typing import List, Dict, Optional, Callable
 from pathlib import Path
 from dataclasses import dataclass
-from enum import Enum
 import time
 import numpy as np
 from aicsimageio.aics_image import AICSImage
-from PyQt5.QtCore import QObject, QThreadPool, QRunnable, pyqtSignal
+from PyQt5.QtCore import QThreadPool, QRunnable
 
 
 @dataclass
@@ -17,7 +16,15 @@ class ImageData:
 
 
 class Worker(QRunnable):
+    """
+    Generic implementation of QRunnable that simply runs the
+    provided do_work function.
+    """
+
     def __init__(self, do_work: Callable):
+        """
+        :param do_work: function that this worker will call in a thread
+        """
         super().__init__()
         self._do_work = do_work
 
@@ -26,12 +33,23 @@ class Worker(QRunnable):
 
 
 class CurationImageLoader:
+    """
+    CurationImageLoader manages image data for curation with the invariant
+    that the getter functions will never be blocking.
+    """
+
     def __init__(
         self,
         raw_images: List[Path],
         seg1_images: List[Path],
         seg2_images: Optional[List[Path]] = None,
     ):
+        """
+        Raises ValueError if provided lists are unequal in size or empty.
+        :param raw_images: paths to raw images
+        :param seg1_images: paths to segmentations
+        :param seg2_images: (optional) paths to second segmentations
+        """
         if len(raw_images) != len(seg1_images) or (
             seg2_images and len(seg1_images) != len(seg2_images)
         ):
@@ -40,12 +58,20 @@ class CurationImageLoader:
             raise ValueError("cannot load images from empty image list")
 
         self._num_images = len(raw_images)
+
         self._raw_images: List[Path] = list(raw_images)
         self._seg1_images: List[Path] = list(seg1_images)
-        self._seg2_images: Optional[List[Path]] = list(seg2_images)
+        self._seg2_images: Optional[List[Path]] = (
+            list(seg2_images) if seg2_images else None
+        )
+
+        # private invariant: _next_img_data will only have < _num_data_dict_keys keys if there is
+        # no next image or a thread is currently updating _next_img_data. Same goes for _prev_img_data
+        self._num_data_dict_keys: int = 3 if self._seg2_images else 2
         self._curr_img_data: Dict[str, ImageData] = {}
         self._next_img_data: Dict[str, ImageData] = {}
         self._prev_img_data: Dict[str, ImageData] = {}
+
         self._cursor: int = 0
         # grab data for first images synchronously, start thread for next images
         self._curr_img_data["raw"] = self._get_image_data(self._raw_images[0])
@@ -57,7 +83,7 @@ class CurationImageLoader:
                 self._seg2_images[0]
             )
 
-        if self._num_images > 1:
+        if self.has_next():
             self._start_extraction_threads(1, self._next_img_data)
 
     def _get_image_data(self, img_path: Path) -> ImageData:
@@ -74,7 +100,7 @@ class CurationImageLoader:
 
     def _start_extraction_threads(
         self, img_index: int, data_dict: Dict[str, ImageData]
-    ):
+    ) -> None:
         data_dict.clear()
         raw_worker: Worker = Worker(
             lambda: self._update_data_dict(
@@ -96,41 +122,60 @@ class CurationImageLoader:
         QThreadPool.globalInstance().start(raw_worker)
         QThreadPool.globalInstance().start(seg1_worker)
 
-    def _wait_on_data_dicts(self):
-        expected_length: int = 3 if self._seg2_images else 2
-        if self._cursor > 0:
+    def _wait_on_data_dicts(self) -> None:
+        """
+        Wait for any ongoing updates to prev and next data dicts to finish.
+        """
+        expected_length: int = self._num_data_dict_keys
+        if self.has_prev():
             while len(self._prev_img_data) < expected_length:
                 time.sleep(0.1)
-                print("waiting prev")
-        if self._cursor < self._num_images - 1:
+        if self.has_next():
             while len(self._next_img_data) < expected_length:
                 time.sleep(0.1)
-                print("waiting next")
 
-    def get_raw_image_data(self):
+    def get_raw_image_data(self) -> ImageData:
         return self._curr_img_data["raw"]
 
-    def get_seg1_image_data(self):
+    def get_seg1_image_data(self) -> ImageData:
         return self._curr_img_data["seg1"]
 
-    def get_seg2_image_data(self):
+    def get_seg2_image_data(self) -> ImageData:
         return self._curr_img_data["seg2"]
 
-    def next(self):
-        if self._cursor == self._num_images - 1:
+    def has_next(self) -> bool:
+        """
+        Returns true iff next() can be safely called.
+        """
+        return self._cursor + 1 < self._num_images
+
+    def has_prev(self) -> bool:
+        """
+        Returns true iff prev() can be safely called.
+        """
+        return self._cursor > 0
+
+    def next(self) -> None:
+        """
+        Advance to the next set of images in this image loader.
+        """
+        if not self.has_next():
             raise RuntimeError("cannot move cursor past end of image lists")
         self._wait_on_data_dicts()
         self._prev_img_data = self._curr_img_data
         self._curr_img_data = self._next_img_data
         self._next_img_data = {}
         self._cursor += 1
-        if self._cursor + 1 < self._num_images:
+        if self.has_next():
             self._start_extraction_threads(
                 self._cursor + 1, self._next_img_data
             )
 
-    def prev(self):
-        if self._cursor == 0:
+    def prev(self) -> None:
+        """
+        Move to the previous set of images in this image loader.
+        """
+        if not self.has_prev():
             raise RuntimeError(
                 "cannot move cursor before beginning of image lists"
             )
@@ -139,7 +184,7 @@ class CurationImageLoader:
         self._curr_img_data = self._prev_img_data
         self._prev_img_data = {}
         self._cursor -= 1
-        if self._cursor > 0:
+        if self.has_prev():
             self._start_extraction_threads(
                 self._cursor - 1, self._prev_img_data
             )
