1a600528fa2211c6673fb792f89a1abb9c95d16a
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-03-21 11:56:07 -0700

    added interfaces for testing

---
+++ src/allencell_ml_segmenter/core/image_data_extractor/aics_image_data_extractor.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from allencell_ml_segmenter.core.image_data_extractor import (
+    IImageDataExtractor,
+    ImageData,
+)
+from aicsimageio.aics_image import AICSImage
+
+
+class AICSImageDataExtractor(IImageDataExtractor):
+    """
+    Extracts image data using aicsimageio.
+    """
+
+    _instance = None
+
+    def extract_image_data(
+        self, img_path: Path, dims: bool = True, np_data: bool = True
+    ) -> ImageData:
+        aics_img: AICSImage = AICSImage(img_path)
+        return ImageData(
+            aics_img.dims.X if dims else None,
+            aics_img.dims.Y if dims else None,
+            aics_img.dims.Z if dims else None,
+            aics_img.dims.C if dims else None,
+            aics_img.data if np_data else None,
+            img_path,
+        )
+
+    @classmethod
+    def global_instance(cls):
+        if cls._instance is None:
+            cls._instance = super(AICSImageDataExtractor, cls).__new__(cls)
+        return cls._instance
