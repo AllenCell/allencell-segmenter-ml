84a69c46b48bb1a43e8a4fbd1833ed127d1cf194
brian.kim <yrkim98@uw.edu>
2024-03-27 11:09:21 -0700

    sort alphabetically

--- src/allencell_ml_segmenter/utils/file_utils.py
+++ src/allencell_ml_segmenter/utils/file_utils.py
@@ -6,7 +6,8 @@ class FileUtils:
 
     @staticmethod
     def get_all_files_in_dir_ignore_hidden(dir_path: Path) -> List[Path]:
-        all_files: List[Path] = list(dir_path.glob("*.*"))
+        # sort alphabetically- default sorting behavior for glob
+        all_files: List[Path] = list(sorted(dir_path.glob("*.*")))
         # Ignore hidden files (such as .DS_Store on mac)
         # There's no way to do this with Path.glob filtering or methods so using list comprehension
         return [file for file in all_files if not file.name.startswith(".")]
