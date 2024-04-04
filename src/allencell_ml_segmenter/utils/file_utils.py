7e4d7482ae343a64de6996d720274be3df6e5c00
brian.kim <yrkim98@uw.edu>
2024-04-04 09:49:47 -0700

    ignore hidden directory- and move into fileutils

--- src/allencell_ml_segmenter/utils/file_utils.py
+++ src/allencell_ml_segmenter/utils/file_utils.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List
+from typing import List, Generator
 
 
 class FileUtils:
@@ -11,3 +11,19 @@ class FileUtils:
         # Ignore hidden files (such as .DS_Store on mac)
         # There's no way to do this with Path.glob filtering or methods so using list comprehension
         return [file for file in all_files if not file.name.startswith(".")]
+
+    @staticmethod
+    def get_img_path_from_folder(folder: Path) -> Path:
+        """
+        Returns path of an image in the folder.
+        :param folder: path to a folder containing images
+        """
+        # we expect user to have the same number of channels for all images in their folders
+        # and that only images are stored in those folders
+
+        path_generator: Generator[Path] = folder.glob("*.*")
+        image: Path = next(path_generator)
+        # ignore hidden files
+        while str(image.name).startswith("."):
+            image: Path = next(path_generator)
+        return image.resolve()
