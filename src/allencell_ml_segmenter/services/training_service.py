a711686d9fc3dc8cbded96a4c84a5b0d7737f5c0
brian.kim <yrkim98@uw.edu>
2024-04-04 15:14:13 -0700

    create cyto_dl_overrides_manager.py

--- src/allencell_ml_segmenter/services/training_service.py
+++ src/allencell_ml_segmenter/services/training_service.py
@@ -12,6 +12,8 @@ from allencell_ml_segmenter.training.training_model import TrainingModel
 from typing import Dict, Union, Optional
 from napari.utils.notifications import show_warning
 
+from allencell_ml_segmenter.utils.cyto_overrides_manager import CytoDLOverridesManager
+
 
 class TrainingService(Subscriber):
     """
@@ -42,20 +44,23 @@ class TrainingService(Subscriber):
         #  https://github.com/AllenCell/allencell-ml-segmenter/issues/156
         if self._able_to_continue_training():
             model = CytoDLModel()
-            # model.download_example_data()
             model.load_default_experiment(
                 self._training_model.get_experiment_type().value,
                 output_dir=f"{self._experiments_model.get_user_experiments_path()}/{self._experiments_model.get_experiment_name()}",
             )
-            model.override_config(self._build_overrides())
+            cyto_overrides_manager: CytoDLOverridesManager = CytoDLOverridesManager(self._experiments_model, self._training_model)
+            model.override_config(cyto_overrides_manager.get_training_overrides())
             model.print_config()
-            asyncio.run(model._train_async())
+            asyncio.run(model.train(run_async=True))
 
     def _able_to_continue_training(self) -> bool:
         if self._experiments_model.get_experiment_name() is None:
             show_warning(
                 "Please select an experiment before running prediction."
             )
+            return False
+
+        if self._training_model.get_experiment_type() is None:
             return False
 
         if self._training_model.get_spatial_dims() is None:
@@ -85,91 +90,3 @@ class TrainingService(Subscriber):
             )
             return False
         return True
-
-    def _hardware_override(self, overrides: Dict[str, Union[str, int, float, bool, Dict]]) -> None:
-        """
-        Get the hardware override for the CytoDLModel
-        """
-        # V1 defaults to CPU
-        overrides["trainer.accelerator"] = "cpu"
-        if self._training_model.get_hardware_type() == Hardware.GPU:
-            overrides["trainer.accelerator"] = "gpu"
-
-    def _spatial_dims_override(self, overrides: Dict[str, Union[str, int, float, bool, Dict]]) -> None:
-        """
-        Get the spatial_dims override for the CytoDlModel
-        """
-        overrides["spatial_dims"] = (
-            self._training_model.get_spatial_dims()
-        )
-
-    def _experiment_name_override(self, overrides: Dict[str, Union[str, int, float, bool, Dict]]) -> None:
-        """
-        Get the experiment name override for the CytoDlModel
-        """
-        overrides["experiment_name"] = (
-            self._experiments_model.get_experiment_name()
-        )
-
-    def _max_run_override(self, overrides: Dict[str, Union[str, int, float, bool, Dict]]) -> None:
-        """
-        Get the max epoch or time override for the CytoDlModel
-        """
-        # define max run (in epochs)
-        overrides["trainer.max_epochs"] = (
-            self._training_model.get_max_epoch()
-        )
-        # max run in time is also defined
-        if self._training_model.use_max_time():
-            # define max runtime (in hours)
-            overrides["trainer.max_time"] = {
-                "minutes": self._training_model.get_max_time()
-            }
-
-    def _images_directory_override(self, overrides: Dict[str, Union[str, int, float, bool, Dict]]) -> None:
-        """
-        Get the data path override for the CytoDlModel
-        Cyto dl expects a train.csv, valid.csv, and a test.csv in this folder for training.
-        """
-        overrides["data.path"] = str(
-            self._training_model.get_images_directory()
-        )
-
-    #
-    def _patch_shape_override(self, overrides: Dict[str, Union[str, int, float, bool, Dict]]) -> None:
-        """
-        get the patch shape override for the CytoDLModel
-        """
-        overrides["data._aux.patch_shape"] = (
-            self._training_model.get_patch_size().value
-        )
-
-    def _checkpoint_override(self, overrides: Dict[str, Union[str, int, float, bool, Dict]]) -> None:
-        """
-        Get the checkpoint path override for the CytoDLModel
-        """
-        if self._experiments_model.get_checkpoint() is not None:
-            # We are going to continue training on an existing model
-            overrides["ckpt_path"] = str(
-                self._experiments_model.get_model_checkpoints_path(
-                    self._experiments_model.get_experiment_name(),
-                    self._experiments_model.get_checkpoint(),
-                )
-            )
-
-    def _build_overrides(self) -> Dict[str, Union[str, int, float, bool, Dict]]:
-        """
-        Build a list of overrides for the CytoDLModel from plugin state.
-        """
-        overrides_dict: Dict[str, Union[str, int, float, bool, Dict]] = dict()
-        # TODO: Add channel index selection from UI
-        # TODO: Add max time from UI
-        # do overrides based on user selections
-        self._hardware_override(overrides_dict)
-        self._spatial_dims_override(overrides_dict)
-        self._max_run_override(overrides_dict)
-        self._images_directory_override(overrides_dict)
-        self._patch_shape_override(overrides_dict)
-        self._checkpoint_override(overrides_dict)
-
-        return overrides_dict
