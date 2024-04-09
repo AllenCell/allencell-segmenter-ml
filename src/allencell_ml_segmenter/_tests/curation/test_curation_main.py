d109b21ac804940d7319ad1535d9d4a3a49bab07
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-04-09 15:11:40 -0700

    using namedtuple

--- src/allencell_ml_segmenter/_tests/curation/test_curation_main.py
+++ src/allencell_ml_segmenter/_tests/curation/test_curation_main.py
@@ -1,4 +1,5 @@
 import pytest
+from collections import namedtuple
 from typing import Tuple
 from allencell_ml_segmenter.curation.main_view import CurationMainView
 from allencell_ml_segmenter._tests.fakes.fake_experiments_model import (
@@ -23,10 +24,11 @@ IMG_DIR_PATH = (
     / "img_folder"
 )
 
+TestEnvironment = namedtuple("TestEnvironment", ["viewer", "model", "service", "view"])
 
-def get_test_instances(
+def get_test_environment(
     include_seg_2: bool,
-) -> Tuple[FakeViewer, CurationModel, CurationService, CurationMainView]:
+) -> TestEnvironment:
     curation_model = CurationModel()
 
     curation_model.set_raw_directory(IMG_DIR_PATH)
@@ -43,14 +45,13 @@ def get_test_instances(
     )
     viewer = FakeViewer()
     curation_service = CurationService(curation_model, viewer)
-    main_view = CurationMainView(curation_model, curation_service)
-    # Note: we assume that when CurationMainView is shown, curation_setup will be called
-
     # with our current setup, need to mock this since this will set up a real
     # image loader and start trying to load images into memory
-    main_view._curation_service.curation_setup = Mock()
+    curation_service.curation_setup = Mock()
+    main_view = CurationMainView(curation_model, curation_service)
+    # Note: we assume that when CurationMainView is shown, curation_setup will be called
     main_view.curation_setup(first_setup=True)
-    return (
+    return TestEnvironment(
         viewer,
         curation_model,
         curation_service,
@@ -60,90 +61,90 @@ def get_test_instances(
 
 def test_initial_state_with_seg2(qtbot: QtBot) -> None:
     # Arrange
-    viewer, model, service, view = get_test_instances(True)
+    env = get_test_environment(True)
 
     # Act
 
     # Assert
-    assert view.merging_create_button.isEnabled()
-    assert not view.merging_save_button.isEnabled()
-    assert view.merging_base_combo.isEnabled()
-    assert view.merging_delete_button.isEnabled()
+    assert env.view.merging_create_button.isEnabled()
+    assert not env.view.merging_save_button.isEnabled()
+    assert env.view.merging_base_combo.isEnabled()
+    assert env.view.merging_delete_button.isEnabled()
 
     # why are these buttons disabled until we create a merging mask?
-    assert not view.excluding_create_button.isEnabled()
-    assert not view.excluding_save_button.isEnabled()
-    assert not view.excluding_delete_button.isEnabled()
+    assert not env.view.excluding_create_button.isEnabled()
+    assert not env.view.excluding_save_button.isEnabled()
+    assert not env.view.excluding_delete_button.isEnabled()
 
-    assert view.progress_bar.value() == 1
-    assert view.progress_bar.maximum() == 3
+    assert env.view.progress_bar.value() == 1
+    assert env.view.progress_bar.maximum() == 3
 
 
 def test_initial_state_no_seg2(qtbot: QtBot) -> None:
     # Arrange
-    viewer, model, service, view = get_test_instances(False)
+    env = get_test_environment(False)
 
     # Act
 
     # Assert
-    assert not view.merging_create_button.isEnabled()
-    assert not view.merging_save_button.isEnabled()
-    assert not view.merging_base_combo.isEnabled()
-    assert not view.merging_delete_button.isEnabled()
+    assert not env.view.merging_create_button.isEnabled()
+    assert not env.view.merging_save_button.isEnabled()
+    assert not env.view.merging_base_combo.isEnabled()
+    assert not env.view.merging_delete_button.isEnabled()
 
-    assert view.excluding_create_button.isEnabled()
-    assert not view.excluding_save_button.isEnabled()
-    assert view.excluding_delete_button.isEnabled()
+    assert env.view.excluding_create_button.isEnabled()
+    assert not env.view.excluding_save_button.isEnabled()
+    assert env.view.excluding_delete_button.isEnabled()
 
-    assert view.progress_bar.value() == 1
-    assert view.progress_bar.maximum() == 3
+    assert env.view.progress_bar.value() == 1
+    assert env.view.progress_bar.maximum() == 3
 
 
 def test_next_image_with_seg2(qtbot: QtBot) -> None:
     # Arrange
-    viewer, model, service, view = get_test_instances(True)
+    env = get_test_environment(True)
 
     # Act
     # 1 = left click: https://het.as.utexas.edu/HET/Software/html/qt.html#MouseButton-enum
-    qtbot.mouseClick(view.next_button, 1)
+    qtbot.mouseClick(env.view.next_button, 1)
 
     # Assert
-    assert view.merging_create_button.isEnabled()
-    assert not view.merging_save_button.isEnabled()
-    assert view.merging_base_combo.isEnabled()
-    assert view.merging_delete_button.isEnabled()
+    assert env.view.merging_create_button.isEnabled()
+    assert not env.view.merging_save_button.isEnabled()
+    assert env.view.merging_base_combo.isEnabled()
+    assert env.view.merging_delete_button.isEnabled()
 
-    assert not view.excluding_create_button.isEnabled()
-    assert not view.excluding_save_button.isEnabled()
-    assert not view.excluding_delete_button.isEnabled()
+    assert not env.view.excluding_create_button.isEnabled()
+    assert not env.view.excluding_save_button.isEnabled()
+    assert not env.view.excluding_delete_button.isEnabled()
 
-    assert view.progress_bar.value() == 2
+    assert env.view.progress_bar.value() == 2
 
-    assert "[raw] raw 2" in viewer.images_added
-    assert "[seg1] seg1 2" in viewer.images_added
-    assert "[seg2] seg2 2" in viewer.images_added
+    assert "[raw] raw 2" in env.viewer.images_added
+    assert "[seg1] seg1 2" in env.viewer.images_added
+    assert "[seg2] seg2 2" in env.viewer.images_added
 
 
 def test_next_image_no_seg2(qtbot: QtBot) -> None:
     # Arrange
-    viewer, model, service, view = get_test_instances(False)
+    env = get_test_environment(False)
 
     # Act
     # 1 = left click: https://het.as.utexas.edu/HET/Software/html/qt.html#MouseButton-enum
-    qtbot.mouseClick(view.next_button, 1)
+    qtbot.mouseClick(env.view.next_button, 1)
 
     # Assert
-    assert not view.merging_create_button.isEnabled()
-    assert not view.merging_save_button.isEnabled()
-    assert not view.merging_base_combo.isEnabled()
-    assert not view.merging_delete_button.isEnabled()
+    assert not env.view.merging_create_button.isEnabled()
+    assert not env.view.merging_save_button.isEnabled()
+    assert not env.view.merging_base_combo.isEnabled()
+    assert not env.view.merging_delete_button.isEnabled()
 
-    assert view.excluding_create_button.isEnabled()
-    assert not view.excluding_save_button.isEnabled()
-    assert view.excluding_delete_button.isEnabled()
+    assert env.view.excluding_create_button.isEnabled()
+    assert not env.view.excluding_save_button.isEnabled()
+    assert env.view.excluding_delete_button.isEnabled()
 
-    assert view.progress_bar.value() == 2
+    assert env.view.progress_bar.value() == 2
 
-    assert "[raw] raw 2" in viewer.images_added
-    assert "[seg1] seg1 2" in viewer.images_added
-    assert "[seg2] seg2 2" in viewer.images_added
+    assert "[raw] raw 2" in env.viewer.images_added
+    assert "[seg1] seg1 2" in env.viewer.images_added
+    assert "[seg2] seg2 2" in env.viewer.images_added
