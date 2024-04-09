be870aaf307063bed50f8d3a1345fbbc36776091
brian.kim <yrkim98@uw.edu>
2024-04-09 12:20:01 -0700

    type of class

--- src/allencell_ml_segmenter/_tests/fakes/fake_image_selection_widget.py
+++ src/allencell_ml_segmenter/_tests/fakes/fake_image_selection_widget.py
@@ -1,10 +1,14 @@
 from typing import Optional
 
+from PyQt5.QtWidgets import QWidget
+
+from allencell_ml_segmenter.core.event import Event
+from allencell_ml_segmenter.core.subscriber import Subscriber
 from allencell_ml_segmenter.main.experiments_model import ExperimentsModel
 from allencell_ml_segmenter.training.training_model import TrainingModel
 
 
-class FakeImageSelectionWidget:
+class FakeImageSelectionWidget(Subscriber):
     """
     A widget for training image selection.
     """
@@ -17,6 +21,12 @@ class FakeImageSelectionWidget:
         self._model: TrainingModel = model
         self._experiments_model: ExperimentsModel = experiments_model
         self.channels_updated_with_max: Optional[int] = None
+
+        self._model.subscribe(
+            Event.ACTION_TRAINING_MAX_NUMBER_CHANNELS_SET,
+            self,
+            self._update_channels,
+        )
 
     def _update_channels(self) -> None:
         self.channels_updated_with_max = self._model.get_max_channel()
