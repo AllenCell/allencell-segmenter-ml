a7f7d6b9a6794d8faa468115a2bb785a3ecab3c1
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-04-10 12:41:54 -0700

    fully reverted training service changes

--- src/allencell_ml_segmenter/services/training_service.py
+++ src/allencell_ml_segmenter/services/training_service.py
@@ -88,7 +88,6 @@ class TrainingService(Subscriber):
         if self._training_model.get_max_epoch() is None:
             show_warning("Please define max epoch(s) to run for")
             return False
-        
         if (
             self._training_model.get_max_channels() > 0
             and self._training_model.get_channel_index() is None
