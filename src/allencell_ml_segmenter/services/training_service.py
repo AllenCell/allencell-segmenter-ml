b3e30e3390667ae5a5fcf86703d6b44ea64dcedd
brian.kim <yrkim98@uw.edu>
2024-04-11 13:32:31 -0700

    IExtractorFactory type

--- src/allencell_ml_segmenter/services/training_service.py
+++ src/allencell_ml_segmenter/services/training_service.py
@@ -8,7 +8,6 @@ from allencell_ml_segmenter.core.channel_extraction import (
     ChannelExtractionThread,
     get_img_path_from_csv,
 )
-from allencell_ml_segmenter.core.extractor_factory import ExtractorFactory
 from allencell_ml_segmenter.core.i_extractor_factory import IExtractorFactory
 from allencell_ml_segmenter.core.subscriber import Subscriber
 from allencell_ml_segmenter.core.event import Event
@@ -46,7 +45,7 @@ class TrainingService(Subscriber):
             self,
             self._train_model_handler,
         )
-        self._extractor_factory = extractor_factory
+        self._extractor_factory: IExtractorFactory = extractor_factory
         self._channel_extraction_thread: Optional[ChannelExtractionThread] = (
             None
         )
