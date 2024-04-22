d2c4aa44a102866b16e27486e0dc4141fed819b3
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-04-22 12:58:09 -0700

    prog bar working

--- src/allencell_ml_segmenter/training/metrics_csv_event_handler.py
+++ src/allencell_ml_segmenter/training/metrics_csv_event_handler.py
@@ -12,27 +12,28 @@ class MetricsCSVEventHandler(FileSystemEventHandler):
     """
 
     def __init__(
-        self, target_path: Path, progress_callback: Callable, min_epoch: int
+        self, target_path: Path, progress_callback: Callable
     ):
         super().__init__()
         self._target_path: Path = target_path
         self._progress_callback: Callable = progress_callback
-        self._min_epoch: int = min_epoch
 
-    def _get_latest_epoch(self) -> int:
-        latest: int = self._min_epoch
+    def _get_num_epochs(self) -> int:
         with self._target_path.open("r", newline="") as fr:
             dict_reader: DictReader = DictReader(fr)
-            for row in dict_reader:
-                latest = (
-                    int(row["epoch"]) if int(row["epoch"]) > latest else latest
-                )
+            epochs = [int(row["epoch"]) for row in dict_reader]
 
-        return latest
+        return len(set(epochs))
 
     # override
     def on_any_event(self, event: FileSystemEvent) -> None:
         if self._target_path.exists() and self._target_path.samefile(
             event.src_path
         ):
-            self._progress_callback(self._get_latest_epoch())
+            # this is a consequence of a cyto-dl quirk: the test metrics are
+            # logged as the n+1'th epoch, even though it uses the n'th epoch's
+            # parameters. Test metrics are calculated at the end of training,
+            # so we want it to appear that training is complete once the metrics
+            # are calculated.
+            output: int = max(0, self._get_num_epochs() - 1)
+            self._progress_callback(output)
