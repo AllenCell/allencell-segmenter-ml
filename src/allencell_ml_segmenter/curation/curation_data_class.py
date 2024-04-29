1c7d624371778de67c7de03c0f3262c6236f5499
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-04-29 13:00:45 -0700

    fix datatype in curation record

--- src/allencell_ml_segmenter/curation/curation_data_class.py
+++ src/allencell_ml_segmenter/curation/curation_data_class.py
@@ -2,7 +2,6 @@ import numpy as np
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
-from napari.layers import Shapes
 
 
 @dataclass
@@ -11,6 +10,6 @@ class CurationRecord:
     seg1: Path
     seg2: Optional[Path]
     excluding_mask: Optional[np.ndarray]
-    merging_mask: Optional[Shapes]
+    merging_mask: Optional[np.ndarray]
     base_image_index: str
     to_use: bool
