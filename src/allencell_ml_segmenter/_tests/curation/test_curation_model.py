3a10b53cb58daf34a2bf13c24ec6b84fa42bba02
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-05-02 17:34:33 -0700

    notes on curation service

--- src/allencell_ml_segmenter/_tests/curation/test_curation_model.py
+++ src/allencell_ml_segmenter/_tests/curation/test_curation_model.py
@@ -3,76 +3,60 @@ from typing import List
 import pytest
 from pathlib import Path
 from unittest.mock import Mock
-from napari.layers import Shapes
+import numpy as np
 
 from allencell_ml_segmenter.core.event import Event
 from allencell_ml_segmenter.curation.curation_data_class import CurationRecord
-from allencell_ml_segmenter.curation.curation_model import CurationModel
-from unittest.mock import patch
+from allencell_ml_segmenter.curation.curation_model import CurationModel, CurationView
+from allencell_ml_segmenter._tests.fakes.fake_experiments_model import FakeExperimentsModel
+from allencell_ml_segmenter.curation.curation_image_loader import FakeCurationImageLoaderFactory
 
 
 @pytest.fixture
 def curation_model() -> CurationModel:
-    return CurationModel()
+    return CurationModel(FakeExperimentsModel(), FakeCurationImageLoaderFactory())
 
 
 def test_set_raw_directory(curation_model: CurationModel) -> None:
     # Arrange
     directory: Path = Path("fake_path")
+    dir_set_slot: Mock = Mock()
+    curation_model.raw_directory_set.connect(dir_set_slot)
 
     # Act
     curation_model.set_raw_directory(directory)
 
     # Assert
     assert curation_model.get_raw_directory() == directory
-
-
-def test_get_raw_directory() -> None:
-    # Arrange
-    test_path: Path = Path("test_raw_path")
-    model = CurationModel(raw_path=test_path)
-
-    # Act / Assert
-    assert model.get_raw_directory() == test_path
+    dir_set_slot.assert_called_once()
 
 
 def test_set_seg1_directory(curation_model: CurationModel) -> None:
     # Arrange
     directory: Path = Path("fake_path")
+    dir_set_slot: Mock = Mock()
+    curation_model.seg1_directory_set.connect(dir_set_slot)
+
     # Act
     curation_model.set_seg1_directory(directory)
 
     # Assert
     assert curation_model.get_seg1_directory() == directory
-
-
-def test_get_seg1_directory() -> None:
-    # Arrange
-    test_path: Path = Path("test_seg1_path")
-    model = CurationModel(seg1_path=test_path)
-
-    # Act / Assert
-    assert model.get_seg1_directory() == test_path
+    dir_set_slot.assert_called_once()
 
 
 def test_set_seg2_directory(curation_model: CurationModel) -> None:
     # Arrange
     directory: Path = Path("fake_path")
+    dir_set_slot: Mock = Mock()
+    curation_model.seg2_directory_set.connect(dir_set_slot)
 
     # Act
     curation_model.set_seg2_directory(directory)
 
     # Assert
     assert curation_model.get_seg2_directory() == directory
-
-
-def test_get_seg2_directory() -> None:
-    # Arrange
-    test_path: Path = Path("test_seg2_path")
-    model = CurationModel(seg2_path=test_path)
-
-    # Act / Assert
-    assert model.get_seg2_directory() == test_path
+    dir_set_slot.assert_called_once()
 
 
 def test_set_raw_channel(curation_model: CurationModel) -> None:
@@ -108,150 +92,76 @@ def test_set_seg2_channel(curation_model: CurationModel) -> None:
     assert curation_model.get_seg2_channel() == channel
 
 
-def test_set_view(curation_model: CurationModel) -> None:
+def test_set_current_view_to_main_view(curation_model: CurationModel) -> None:
     # Arrange
-    with patch.object(CurationModel, "dispatch") as dispatch_mock:
-        # Act
-        curation_model.set_view()
 
-        # Assert
-        dispatch_mock.assert_called_once_with(
-            Event.PROCESS_CURATION_INPUT_STARTED
-        )
+    # necessary for init of image loader
+    curation_model.set_raw_directory_paths([Path("r1"), Path("r2"), Path("r3")])
+    curation_model.set_seg1_directory_paths([Path("s11"), Path("s12"), Path("s13")])
+    curation_model.set_seg2_directory_paths([Path("s21"), Path("s22"), Path("s23")])
 
+    view_changed_slot: Mock = Mock()
+    curation_model.current_view_changed.connect(view_changed_slot)
 
-def test_get_merging_mask_base_layer(curation_model: CurationModel) -> None:
-    # Assert
-    assert curation_model.get_merging_mask_base_layer() is None
+    first_image_ready_slot: Mock = Mock()
+    curation_model.first_image_data_ready.connect(first_image_ready_slot)
 
-
-def test_set_merging_mask_base_layer(curation_model: CurationModel) -> None:
-    # arrange
-    curation_model.set_merging_mask_base_layer("layer_name")
-
-    # Act/Assert
-    assert curation_model.get_merging_mask_base_layer() == "layer_name"
-
-
-def test_get_curation_image_dims(curation_model: CurationModel) -> None:
-    # Assert
-    assert curation_model.get_curation_image_dims() is None
-
-
-def test_set_curation_image_dims(curation_model: CurationModel) -> None:
-    # Arrange
-    curation_model.set_curation_image_dims((100, 200, 3))
-
-    # Act/Assert
-    assert curation_model.get_curation_image_dims() == (100, 200, 3)
-
-
-def test_set_current_merging_mask_path(curation_model: CurationModel) -> None:
-    # Act
-    curation_model.set_current_merging_mask_path(Path("mask_path"))
-
-    # Assert
-    assert curation_model.get_current_merging_mask_path() == Path("mask_path")
-
-
-def test_get_excluding_mask_shape_layers(
-    curation_model: CurationModel,
-) -> None:
-    # Arrange
-    sample_shape_layers: List[Shapes] = [
-        Mock(spec=Shapes),
-        Mock(spec=Shapes),
-        Mock(spec=Shapes),
-        Mock(spec=Shapes),
-    ]
-    curation_model.set_excluding_mask_shape_layers(sample_shape_layers)
-
-    # Act/Assert
-    assert (
-        curation_model.get_excluding_mask_shape_layers() == sample_shape_layers
-    )
-
-
-def test_set_excluding_mask_shape_layers(
-    curation_model: CurationModel,
-) -> None:
-    # Arrange
-    layers: List[Shapes] = [Shapes()]
+    next_image_ready_slot: Mock = Mock()
+    curation_model.next_image_data_ready.connect(next_image_ready_slot)
 
     # Act
-    curation_model.set_excluding_mask_shape_layers(layers)
+    curation_model.set_current_view(CurationView.MAIN_VIEW)
 
     # Assert
-    assert curation_model.get_excluding_mask_shape_layers() == layers
+    assert curation_model.get_current_view() == CurationView.MAIN_VIEW
+
+    view_changed_slot.assert_called_once()
+    first_image_ready_slot.assert_called_once()
+    next_image_ready_slot.assert_called_once() # should be called since > 1 image in dirs
+
+    assert curation_model.get_raw_image_data().path == Path("r1")
+    assert curation_model.get_seg1_image_data().path == Path("s11")
+    assert curation_model.get_seg2_image_data().path == Path("s21")
 
 
-def test_append_excluding_mask_shape_layer(
-    curation_model: CurationModel,
-) -> None:
+# TODO: if we end up needing to change back from main to input view, add tests for that
+# here
+
+def test_set_merging_mask(curation_model: CurationModel) -> None:
     # Arrange
-    layer: Shapes = Shapes()
-
+    mask: np.ndarray = np.asarray([[1, 2], [3, 4]])
     # Act
-    curation_model.append_excluding_mask_shape_layer(layer)
-
+    curation_model.set_merging_mask(mask)
     # Assert
-    assert curation_model.get_excluding_mask_shape_layers() == [layer]
+    assert np.array_equal(curation_model.get_merging_mask(), mask)
 
-
-def test_get_merging_mask_shape_layers(curation_model: CurationModel) -> None:
+def test_set_excluding_mask(curation_model: CurationModel) -> None:
     # Arrange
-    sample_shape_layers: List[Shapes] = [
-        Mock(spec=Shapes),
-        Mock(spec=Shapes),
-        Mock(spec=Shapes),
-        Mock(spec=Shapes),
-    ]
-    curation_model.set_merging_mask_shape_layers(sample_shape_layers)
-
-    # Act/Assert
-    assert (
-        curation_model.get_merging_mask_shape_layers() == sample_shape_layers
-    )
-
-
-def test_set_merging_mask_shape_layers(curation_model: CurationModel) -> None:
-    # Arrange
-    layers: List[Shapes] = [Shapes()]
-
+    mask: np.ndarray = np.asarray([[8, 2], [3, 5]])
     # Act
-    curation_model.set_merging_mask_shape_layers(layers)
-
+    curation_model.set_excluding_mask(mask)
     # Assert
-    assert curation_model.get_merging_mask_shape_layers() == layers
+    assert np.array_equal(curation_model.get_excluding_mask(), mask)
 
-
-def test_append_merging_mask_shape_layer(
-    curation_model: CurationModel,
-) -> None:
-    # Arrange
-    layer: Shapes = Shapes()
-
+def test_set_base_image(curation_model: CurationModel) -> None:
     # Act
-    curation_model.append_merging_mask_shape_layer(layer)
+    curation_model.set_base_image("seg2")
 
     # Assert
-    assert curation_model.get_merging_mask_shape_layers() == [layer]
+    assert curation_model.get_base_image() == "seg2"
 
-
-def test_append_curation_record(curation_model: CurationModel) -> None:
-    # Arrange
-    record: CurationRecord = CurationRecord(
-        raw_file="raw",
-        seg1="seg1",
-        seg2="seg2",
-        excluding_mask="exclude",
-        merging_mask="merge",
-        base_image_index="seg1",
-        to_use=True,
-    )
-
+def test_set_use_image(curation_model: CurationModel) -> None:
     # Act
-    curation_model.append_curation_record(record)
+    curation_model.set_use_image(False)
 
     # Assert
-    assert curation_model.get_curation_record() == [record]
+    assert not curation_model.get_use_image()
+
+
+
+
+
+
+
+
+
