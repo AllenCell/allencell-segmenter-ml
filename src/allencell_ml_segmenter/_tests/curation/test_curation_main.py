7761534392b11fa531a81dfd7ed26bca6bcd54c2
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-05-07 11:28:06 -0700

    good start on main view tests

--- src/allencell_ml_segmenter/_tests/curation/test_curation_main.py
+++ src/allencell_ml_segmenter/_tests/curation/test_curation_main.py
@@ -2,13 +2,13 @@ from dataclasses import dataclass
 from allencell_ml_segmenter.curation.main_view import CurationMainView
 from allencell_ml_segmenter._tests.fakes.fake_viewer import FakeViewer
 from allencell_ml_segmenter.main.i_viewer import IViewer
-from allencell_ml_segmenter.curation.curation_model import CurationModel
-from allencell_ml_segmenter.curation.curation_service import CurationService
+from allencell_ml_segmenter.curation.curation_model import CurationModel, CurationView
 from allencell_ml_segmenter._tests.fakes.fake_experiments_model import FakeExperimentsModel
 from allencell_ml_segmenter.curation.curation_image_loader import FakeCurationImageLoaderFactory
 import allencell_ml_segmenter
 
 from pytestqt.qtbot import QtBot
+from unittest.mock import Mock
 from pathlib import Path
 import pytest
 
@@ -35,27 +35,61 @@ def test_environment_with_seg2() -> TestEnvironment:
     curation_model.set_raw_directory_paths(IMG_DIR_FILES)
     curation_model.set_seg1_directory_paths(IMG_DIR_FILES)
     curation_model.set_seg2_directory_paths(IMG_DIR_FILES)
+    curation_model.set_current_view(CurationView.MAIN_VIEW)
 
     viewer: IViewer = FakeViewer()
     main_view: CurationMainView = CurationMainView(
         curation_model, viewer
     )
-    # Note: we assume that when CurationMainView is shown, curation_setup will be called
-    main_view.curation_setup(first_setup=True)
+
     return TestEnvironment(
         viewer,
         curation_model,
-        curation_service,
         main_view,
     )
 
+@pytest.fixture
+def test_environment_without_seg2() -> TestEnvironment:
+    curation_model: CurationModel = CurationModel(FakeExperimentsModel(), FakeCurationImageLoaderFactory())
 
-def test_initial_state_with_seg2(qtbot: QtBot) -> None:
-    # Arrange
-    env: TestEnvironment = get_test_environment(True)
+    curation_model.set_raw_directory_paths(IMG_DIR_FILES)
+    curation_model.set_seg1_directory_paths(IMG_DIR_FILES)
+    curation_model.set_current_view(CurationView.MAIN_VIEW)
+
+    viewer: IViewer = FakeViewer()
+    main_view: CurationMainView = CurationMainView(
+        curation_model, viewer
+    )
+    
+    return TestEnvironment(
+        viewer,
+        curation_model,
+        main_view,
+    )
+
+### UI State Tests ----------------------------------------------------------------------------------
+
+def test_initial_state_with_seg2(qtbot: QtBot, test_environment_with_seg2: TestEnvironment) -> None:
+    env: TestEnvironment = test_environment_with_seg2
+
+    # everything should be disabled until signals from model fire
+    # Assert
+    assert not env.view.merging_create_button.isEnabled()
+    assert not env.view.merging_save_button.isEnabled()
+    assert not env.view.merging_base_combo.isEnabled()
+    assert not env.view.merging_delete_button.isEnabled()
+
+    assert not env.view.excluding_create_button.isEnabled()
+    assert not env.view.excluding_delete_button.isEnabled()
+    assert not env.view.excluding_save_button.isEnabled()
+
+    assert not env.view.yes_radio.isEnabled()
+    assert not env.view.no_radio.isEnabled()
+
+    assert not env.view.next_button.isEnabled()
 
     # Act
-
+    env.model.first_image_data_ready.emit()
     # Assert
     assert env.view.merging_create_button.isEnabled()
     assert not env.view.merging_save_button.isEnabled()
@@ -66,46 +100,13 @@ def test_initial_state_with_seg2(qtbot: QtBot) -> None:
     assert env.view.excluding_delete_button.isEnabled()
     assert not env.view.excluding_save_button.isEnabled()
 
-    assert env.view.progress_bar.value() == 1
-    assert env.view.progress_bar.maximum() == 2
+    assert env.view.yes_radio.isEnabled()
+    assert env.view.no_radio.isEnabled()
 
-    assert "[raw] t1.tiff" in env.viewer.images_added
-    assert "[seg1] t1.tiff" in env.viewer.images_added
-    assert "[seg2] t1.tiff" in env.viewer.images_added
-
-
-def test_initial_state_no_seg2(qtbot: QtBot) -> None:
-    # Arrange
-    env: TestEnvironment = get_test_environment(False)
+    assert not env.view.next_button.isEnabled()
 
     # Act
-
-    # Assert
-    assert not env.view.merging_create_button.isEnabled()
-    assert not env.view.merging_save_button.isEnabled()
-    assert not env.view.merging_base_combo.isEnabled()
-    assert not env.view.merging_delete_button.isEnabled()
-
-    assert env.view.excluding_create_button.isEnabled()
-    assert not env.view.excluding_save_button.isEnabled()
-    assert env.view.excluding_delete_button.isEnabled()
-
-    assert env.view.progress_bar.value() == 1
-    assert env.view.progress_bar.maximum() == 2
-
-    assert "[raw] t1.tiff" in env.viewer.images_added
-    assert "[seg1] t1.tiff" in env.viewer.images_added
-    assert "[seg2] t1.tiff" not in env.viewer.images_added
-
-
-def test_next_image_with_seg2(qtbot: QtBot) -> None:
-    # Arrange
-    env: TestEnvironment = get_test_environment(True)
-
-    # Act
-    # 1 = left click: https://het.as.utexas.edu/HET/Software/html/qt.html#MouseButton-enum
-    qtbot.mouseClick(env.view.next_button, 1)
-
+    env.model.next_image_data_ready.emit()
     # Assert
     assert env.view.merging_create_button.isEnabled()
     assert not env.view.merging_save_button.isEnabled()
@@ -113,36 +114,188 @@ def test_next_image_with_seg2(qtbot: QtBot) -> None:
     assert env.view.merging_delete_button.isEnabled()
 
     assert env.view.excluding_create_button.isEnabled()
-    assert not env.view.excluding_save_button.isEnabled()
     assert env.view.excluding_delete_button.isEnabled()
+    assert not env.view.excluding_save_button.isEnabled()
 
-    assert env.view.progress_bar.value() == 2
+    assert env.view.yes_radio.isEnabled()
+    assert env.view.no_radio.isEnabled()
 
-    assert "[raw] t2.tiff" in env.viewer.images_added
-    assert "[seg1] t2.tiff" in env.viewer.images_added
-    assert "[seg2] t2.tiff" in env.viewer.images_added
+    assert env.view.next_button.isEnabled()
+
+    assert f"[raw] {IMG_DIR_FILES[0].name}" in env.viewer.images_added
+    assert f"[seg1] {IMG_DIR_FILES[0].name}" in env.viewer.images_added
+    assert f"[seg2] {IMG_DIR_FILES[0].name}" in env.viewer.images_added
+
+    assert env.view.progress_bar.value() == 1
+    assert env.view.progress_bar.maximum() == 3
 
 
-def test_next_image_no_seg2(qtbot: QtBot) -> None:
-    # Arrange
-    env: TestEnvironment = get_test_environment(False)
+def test_initial_state_no_seg2(qtbot: QtBot, test_environment_without_seg2: TestEnvironment) -> None:
+    env: TestEnvironment = test_environment_without_seg2
 
-    # Act
-    # 1 = left click: https://het.as.utexas.edu/HET/Software/html/qt.html#MouseButton-enum
-    qtbot.mouseClick(env.view.next_button, 1)
-
+    # everything should be disabled until signals from model fire
     # Assert
     assert not env.view.merging_create_button.isEnabled()
     assert not env.view.merging_save_button.isEnabled()
     assert not env.view.merging_base_combo.isEnabled()
     assert not env.view.merging_delete_button.isEnabled()
 
-    assert env.view.excluding_create_button.isEnabled()
+    assert not env.view.excluding_create_button.isEnabled()
+    assert not env.view.excluding_delete_button.isEnabled()
     assert not env.view.excluding_save_button.isEnabled()
-    assert env.view.excluding_delete_button.isEnabled()
 
+    assert not env.view.yes_radio.isEnabled()
+    assert not env.view.no_radio.isEnabled()
+
+    assert not env.view.next_button.isEnabled()
+
+    # Act
+    env.model.first_image_data_ready.emit()
+    # Assert
+    assert not env.view.merging_create_button.isEnabled()
+    assert not env.view.merging_save_button.isEnabled()
+    # assert env.view.merging_base_combo.isEnabled() this change comes in later PR
+    assert not env.view.merging_delete_button.isEnabled()
+
+    assert env.view.excluding_create_button.isEnabled()
+    assert env.view.excluding_delete_button.isEnabled()
+    assert not env.view.excluding_save_button.isEnabled()
+
+    assert env.view.yes_radio.isEnabled()
+    assert env.view.no_radio.isEnabled()
+
+    assert not env.view.next_button.isEnabled()
+
+    # Act
+    env.model.next_image_data_ready.emit()
+    # Assert
+    assert not env.view.merging_create_button.isEnabled()
+    assert not env.view.merging_save_button.isEnabled()
+    # assert env.view.merging_base_combo.isEnabled() this change comes in later PR
+    assert not env.view.merging_delete_button.isEnabled()
+
+    assert env.view.excluding_create_button.isEnabled()
+    assert env.view.excluding_delete_button.isEnabled()
+    assert not env.view.excluding_save_button.isEnabled()
+
+    assert env.view.yes_radio.isEnabled()
+    assert env.view.no_radio.isEnabled()
+
+    assert env.view.next_button.isEnabled()
+
+    assert f"[raw] {IMG_DIR_FILES[0].name}" in env.viewer.images_added
+    assert f"[seg1] {IMG_DIR_FILES[0].name}" in env.viewer.images_added
+    assert not f"[seg2] {IMG_DIR_FILES[0].name}" in env.viewer.images_added
+
+    assert env.view.progress_bar.value() == 1
+    assert env.view.progress_bar.maximum() == 3
+
+
+def test_next_image(qtbot: QtBot, test_environment_with_seg2: TestEnvironment) -> None:
+    # Arrange
+    env: TestEnvironment = test_environment_with_seg2
+    env.model.first_image_data_ready.emit()
+    env.model.next_image_data_ready.emit()
+
+    # Act
+    # 1 = left click: https://het.as.utexas.edu/HET/Software/html/qt.html#MouseButton-enum
+    qtbot.mouseClick(env.view.next_button, 1)
+
+    # Assert
     assert env.view.progress_bar.value() == 2
 
-    assert "[raw] t2.tiff" in env.viewer.images_added
-    assert "[seg1] t2.tiff" in env.viewer.images_added
-    assert "[seg2] t2.tiff" not in env.viewer.images_added
+    assert env.view.merging_create_button.isEnabled()
+    assert not env.view.merging_save_button.isEnabled()
+    assert env.view.merging_base_combo.isEnabled()
+    assert env.view.merging_delete_button.isEnabled()
+
+    assert env.view.excluding_create_button.isEnabled()
+    assert env.view.excluding_delete_button.isEnabled()
+    assert not env.view.excluding_save_button.isEnabled()
+
+    assert env.view.yes_radio.isEnabled()
+    assert env.view.no_radio.isEnabled()
+
+    assert not env.view.next_button.isEnabled()
+    env.model.next_image_data_ready.emit()
+    assert env.view.next_button.isEnabled()
+
+    assert f"[raw] {IMG_DIR_FILES[1].name}" in env.viewer.images_added
+    assert f"[seg1] {IMG_DIR_FILES[1].name}" in env.viewer.images_added
+    assert f"[seg2] {IMG_DIR_FILES[1].name}" in env.viewer.images_added
+
+
+def test_last_image(qtbot: QtBot, test_environment_with_seg2: TestEnvironment) -> None:
+    # Arrange
+    env: TestEnvironment = test_environment_with_seg2
+    env.model.first_image_data_ready.emit()
+    env.model.next_image_data_ready.emit()
+
+    # Act
+    # 1 = left click: https://het.as.utexas.edu/HET/Software/html/qt.html#MouseButton-enum
+    qtbot.mouseClick(env.view.next_button, 1)
+    env.model.next_image_data_ready.emit()
+
+    qtbot.mouseClick(env.view.next_button, 1)
+    # since there is no next image after this click, we do not expect next_image_data_ready signal
+
+    # Assert
+    assert env.view.progress_bar.value() == 3
+
+    assert env.view.merging_create_button.isEnabled()
+    assert not env.view.merging_save_button.isEnabled()
+    assert env.view.merging_base_combo.isEnabled()
+    assert env.view.merging_delete_button.isEnabled()
+
+    assert env.view.excluding_create_button.isEnabled()
+    assert env.view.excluding_delete_button.isEnabled()
+    assert not env.view.excluding_save_button.isEnabled()
+
+    assert env.view.yes_radio.isEnabled()
+    assert env.view.no_radio.isEnabled()
+
+    assert env.view.next_button.isEnabled()
+
+    assert f"[raw] {IMG_DIR_FILES[2].name}" in env.viewer.images_added
+    assert f"[seg1] {IMG_DIR_FILES[2].name}" in env.viewer.images_added
+    assert f"[seg2] {IMG_DIR_FILES[2].name}" in env.viewer.images_added
+
+    # Act
+    qtbot.mouseClick(env.view.next_button, 1) # reached end, so buttons should be disabled
+
+    # Assert
+    assert env.view.progress_bar.value() == 3
+
+    assert not env.view.merging_create_button.isEnabled()
+    assert not env.view.merging_save_button.isEnabled()
+    assert not env.view.merging_base_combo.isEnabled()
+    assert not env.view.merging_delete_button.isEnabled()
+
+    assert not env.view.excluding_create_button.isEnabled()
+    assert not env.view.excluding_delete_button.isEnabled()
+    assert not env.view.excluding_save_button.isEnabled()
+
+    assert not env.view.yes_radio.isEnabled()
+    assert not env.view.no_radio.isEnabled()
+
+    assert not env.view.next_button.isEnabled()
+
+def test_create_new_merging_mask(qtbot: QtBot, test_environment_with_seg2: TestEnvironment) -> None:
+    # Arrange
+    env: TestEnvironment = test_environment_with_seg2
+    env.model.first_image_data_ready.emit()
+    env.model.next_image_data_ready.emit()
+
+    # Act
+    qtbot.mouseClick(env.view.merging_create_button, 1)
+    
+### View + Model Integration Tests ------------------------------------------------------------------------
+
+def test_curation_record_on_next_with_seg2(qtbot: QtBot, test_environment_with_seg2: TestEnvironment) -> None:
+    # Arrange
+    env: TestEnvironment = test_environment_with_seg2
+    env.model.first_image_data_ready.emit()
+    env.model.next_image_data_ready.emit()
+
+    # Act
+    qtbot.mouseClick(env.view.no_radio)
