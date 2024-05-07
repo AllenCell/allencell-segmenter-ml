a9620c89dc242aedd3ae0f764a7b47eb1c80c50a
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-05-07 15:10:30 -0700

    refactor viewer

--- src/allencell_ml_segmenter/main/i_viewer.py
+++ src/allencell_ml_segmenter/main/i_viewer.py
@@ -1,4 +1,10 @@
 from abc import ABC, abstractmethod
+from typing import List, Optional, Callable
+from allencell_ml_segmenter.main.segmenter_layer import ShapesLayer, ImageLayer
+import numpy as np
+from napari.layers import Layer, Shapes
+from napari.layers.shapes.shapes import Mode
+from napari.utils.events import Event as NapariEvent
 
 
 class IViewer(ABC):
@@ -6,29 +12,42 @@ class IViewer(ABC):
         super().__init__()
 
     @abstractmethod
-    def add_image(self, image, name):
+    def add_image(self, image: np.ndarray, name: str) -> None:
+        pass
+    
+    @abstractmethod
+    def get_image(self, name: str) -> Optional[ImageLayer]:
         pass
 
     @abstractmethod
-    def add_shapes(self, name, face_color):
+    def get_all_images(self) -> List[ImageLayer]:
         pass
 
     @abstractmethod
-    def clear_layers(self):
+    def add_shapes(self, name: str, face_color: str, mode: str) -> None:
+        pass
+    
+    @abstractmethod
+    def get_shapes(self, name: str) -> Optional[ShapesLayer]:
         pass
 
     @abstractmethod
-    def clear_mask_layers(self, layers_to_remove):
+    def get_all_shapes(self) -> List[ShapesLayer]:
         pass
 
     @abstractmethod
-    def get_layers(self):
+    def clear_layers(self) -> None:
         pass
 
     @abstractmethod
-    def get_paths_of_image_layers(self):
+    def remove_layer(self, name: str) -> bool:
+        pass
+
+    # TODO: refactor prediction/file_input_widget.py to not use this and use get_all_images instead
+    @abstractmethod
+    def get_layers(self) -> List[Layer]:
         pass
 
     @abstractmethod
-    def subscribe_layers_change_event(self, function):
+    def subscribe_layers_change_event(self, function: Callable[[NapariEvent], None]) -> None:
         pass
