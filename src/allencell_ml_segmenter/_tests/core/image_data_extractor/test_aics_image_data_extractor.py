bf25c705430dfc704e9276a67c1500cf61654f98
brian.kim <yrkim98@uw.edu>
2024-05-16 12:13:14 -0700

    delete copied test

--- src/allencell_ml_segmenter/_tests/core/image_data_extractor/test_aics_image_data_extractor.py
+++ src/allencell_ml_segmenter/_tests/core/image_data_extractor/test_aics_image_data_extractor.py
@@ -31,29 +31,6 @@ def test_extract_image_data_all() -> None:
     assert np.array_equal(image_data.np_data, expected_img_data)
     assert image_data.path == t1_path
 
-
-def test_extract_image_data_all() -> None:
-    # Arrange
-    extractor = AICSImageDataExtractor.global_instance()
-    t1_path: Path = (
-        Path(allencell_ml_segmenter.__file__).parent
-        / "_tests"
-        / "test_files"
-        / "images"
-        / "test_3_channels.tiff"
-    )
-    expected_img_data = AICSImage(t1_path).data
-
-    # Act
-    image_data = extractor.extract_image_data(t1_path, dims=True, np_data=True)
-    assert image_data.channels == 3
-    assert image_data.dim_x == 2
-    assert image_data.dim_y == 2
-    assert image_data.dim_z == 2
-    np.testing.assert_array_equal(image_data.np_data, expected_img_data)
-    assert image_data.path == t1_path
-
-
 def test_extract_image_data_dims_only() -> None:
     # Arrange
     extractor = AICSImageDataExtractor.global_instance()
@@ -64,7 +41,6 @@ def test_extract_image_data_dims_only() -> None:
         / "images"
         / "test_3_channels.tiff"
     )
-    expected_img_data = AICSImage(t1_path).data
 
     # Act
     image_data = extractor.extract_image_data(
