b55d0608ef164555dd4a2b8aaece360a963c3672
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-05-17 16:08:05 -0700

    modify curation loader signals

--- src/allencell_ml_segmenter/curation/curation_image_loader/i_curation_image_loader.py
+++ src/allencell_ml_segmenter/curation/curation_image_loader/i_curation_image_loader.py
@@ -11,12 +11,8 @@ from allencell_ml_segmenter.core.image_data_extractor import (
 
 
 class CurationImageLoaderSignals(QObject):
-    # emitted after a call to next or start, when the loading for 'new next' is done
-    next_image_ready: Signal = Signal()
-    # emitted after a call to prev, when the loading for 'new prev' is done
-    prev_image_ready: Signal = Signal()
-    # emitted after a call to start, when the loading for 'curr' is done
-    first_image_ready: Signal = Signal()
+    # emitted when all currently loading images are completely loaded
+    images_ready: Signal = Signal()
 
 
 class ICurationImageLoader(ABC):
