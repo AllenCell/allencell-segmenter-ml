abbb8653e8734c1bbbe304baa811fb6c3c500b98
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-05-21 15:40:48 -0700

    passed manual testing

--- src/allencell_ml_segmenter/utils/file_utils.py
+++ src/allencell_ml_segmenter/utils/file_utils.py
@@ -1,12 +1,12 @@
 import numpy as np
 from pathlib import Path
 import csv
-from typing import List, Generator
-from napari.layers import Shapes
+from typing import List, Generator, Tuple
 from allencell_ml_segmenter.curation.curation_data_class import CurationRecord
 import os
 import platform
 import subprocess
+import random
 
 
 # TODO: decide whether utils should be like this with staticmethods or singletons with
@@ -50,6 +50,46 @@ class FileUtils:
 
     @staticmethod
     def write_curation_record(
+        curation_record: List[CurationRecord],
+        csv_dir_path: Path,
+        mask_dir_path: Path,
+    ) -> None:
+        """
+        Saves the curation record as a train and test csv in csv_path_dir and associated masks in mask_dir_path
+        :param curation_record: record to save to csv
+        :param csv_dir_path: directory to save csv (csvs will be named train.csv and test.csv)
+        :param mask_dir_path: directory in which to save masks (masks will be saved under excluding_masks or
+        merging_masks subdirs)
+        """
+        train, test = FileUtils._train_test_split(curation_record)
+        FileUtils._write_curation_csv(
+            train, csv_dir_path / "train.csv", mask_dir_path
+        )
+        FileUtils._write_curation_csv(
+            test, csv_dir_path / "test.csv", mask_dir_path
+        )
+
+    @staticmethod
+    def _train_test_split(
+        curation_record: List[CurationRecord],
+    ) -> Tuple[List[CurationRecord], List[CurationRecord]]:
+        """
+        Returns (train_split, test_split) of the items marked to_use in the provided curation record.
+        Attempts to reserve 10% of these for test, but if that value is < 2, it will become 2, and
+        if it is > 100, it will become 100. If there are <= 2 records marked to_use, an empty test
+        split will be provided.
+        :param curation_record: record to split
+        """
+        curation_record = [r for r in curation_record if r.to_use]
+        if len(curation_record) <= 2:
+            return curation_record, []
+
+        test_len: int = max(2, min(100, len(curation_record) // 10))
+        random.shuffle(curation_record)
+        return curation_record[test_len:], curation_record[:test_len]
+
+    @staticmethod
+    def _write_curation_csv(
         curation_record: List[CurationRecord],
         csv_path: Path,
         mask_dir_path: Path,
