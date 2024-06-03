071349233873cc88f7fd7de1378d27ffcb961194
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-06-03 11:03:46 -0700

    mock file dialog

--- src/allencell_ml_segmenter/_tests/curation/test_curation_input_view.py
+++ src/allencell_ml_segmenter/_tests/curation/test_curation_input_view.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
+from unittest.mock import Mock, patch
 
 import pytest
-from qtpy.QtWidgets import QComboBox
+from qtpy.QtWidgets import QComboBox, QFileDialog
 from pytestqt.qtbot import QtBot
 
 from allencell_ml_segmenter.curation.input_view import CurationInputView
@@ -26,66 +27,92 @@ def test_env() -> TestEnvironment:
     return TestEnvironment(model, CurationInputView(model))
 
 
-# TODO: figure out how to test the qfiledialog portion of this view -> will also make
-# testing the start curation button possible
-"""
-@patch.multiple(
-    QFileDialog,
-    exec_=Mock(return_value=QFileDialog.Accepted),
-    selectedFiles=Mock(return_value=[MOCK_PATH]),
-    getExistingDirectory=Mock(return_value=MOCK_PATH),
-)
-"""
+MOCK_STR_PATH: str = "mock_path"
 
 ### UI State Tests ----------------------------------------------------------------------------------
 
+@patch.multiple(
+    QFileDialog,
+    exec_=Mock(return_value=QFileDialog.Accepted),
+    getExistingDirectory=Mock(return_value=MOCK_STR_PATH),
+)
+class TestsWithStubbedFileDialog:
+    def test_raw_dir_selected(self, qtbot: QtBot, test_env: TestEnvironment) -> None:
+        # Assert sanity check
+        assert not test_env.view.raw_dir_stacked_spinner.spinner.is_spinning
+        # Act
+        test_env.view.raw_directory_select.button.click()
+        # Assert
+        assert test_env.view.raw_dir_stacked_spinner.spinner.is_spinning
 
-def test_raw_channel_set(qtbot: QtBot, test_env: TestEnvironment) -> None:
-    # Arrange
-    # Simulate the model's channel count being set
-    count: int = 2
-    combo_box: QComboBox = test_env.view.raw_image_channel_combo
+    def test_seg1_dir_selected(self, qtbot: QtBot, test_env: TestEnvironment) -> None:
+        # Assert sanity check
+        assert not test_env.view.seg1_dir_stacked_spinner.spinner.is_spinning
+        # Act
+        test_env.view.seg1_directory_select.button.click()
+        # Assert
+        assert test_env.view.seg1_dir_stacked_spinner.spinner.is_spinning
+    
+    def test_seg2_dir_selected(self, qtbot: QtBot, test_env: TestEnvironment) -> None:
+        # Assert sanity check
+        assert not test_env.view.seg2_dir_stacked_spinner.spinner.is_spinning
+        # Act
+        test_env.view.seg2_directory_select.button.click()
+        # Assert
+        assert test_env.view.seg2_dir_stacked_spinner.spinner.is_spinning
 
-    # Act
-    test_env.model.set_channel_count(CurationImageType.RAW, count)
+    def test_raw_channel_count_set(self, qtbot: QtBot, test_env: TestEnvironment) -> None:
+        # Arrange
+        test_env.view.raw_directory_select.button.click()
+        # Simulate the model's channel count being set
+        count: int = 2
+        combo_box: QComboBox = test_env.view.raw_image_channel_combo
 
-    # Assert
-    expected_items = [str(x) for x in range(count)]
-    assert combo_box.count() == len(expected_items)
-    for i in range(combo_box.count()):
-        assert combo_box.itemText(i) == expected_items[i]
+        # Act
+        test_env.model.set_channel_count(CurationImageType.RAW, count)
 
-
-def test_seg1_channel_set(qtbot: QtBot, test_env: TestEnvironment) -> None:
-    # Arrange
-    # Simulate the model's channel count being set
-    count: int = 3
-    combo_box: QComboBox = test_env.view.seg1_image_channel_combo
-
-    # Act
-    test_env.model.set_channel_count(CurationImageType.SEG1, count)
-
-    # Assert
-    expected_items = [str(x) for x in range(count)]
-    assert combo_box.count() == len(expected_items)
-    for i in range(combo_box.count()):
-        assert combo_box.itemText(i) == expected_items[i]
+        # Assert
+        assert not test_env.view.raw_dir_stacked_spinner.spinner.is_spinning
+        expected_items = [str(x) for x in range(count)]
+        assert combo_box.count() == len(expected_items)
+        for i in range(combo_box.count()):
+            assert combo_box.itemText(i) == expected_items[i]
 
 
-def test_seg2_channel_set(qtbot: QtBot, test_env: TestEnvironment) -> None:
-    # Arrange
-    # Simulate the model's channel count being set
-    count: int = 4
-    combo_box: QComboBox = test_env.view.seg2_image_channel_combo
+    def test_seg1_channel_count_set(self, qtbot: QtBot, test_env: TestEnvironment) -> None:
+        # Arrange
+        test_env.view.seg1_directory_select.button.click()
+        # Simulate the model's channel count being set
+        count: int = 3
+        combo_box: QComboBox = test_env.view.seg1_image_channel_combo
 
-    # Act
-    test_env.model.set_channel_count(CurationImageType.SEG2, count)
+        # Act
+        test_env.model.set_channel_count(CurationImageType.SEG1, count)
 
-    # Assert
-    expected_items = [str(x) for x in range(count)]
-    assert combo_box.count() == len(expected_items)
-    for i in range(combo_box.count()):
-        assert combo_box.itemText(i) == expected_items[i]
+        # Assert
+        assert not test_env.view.seg1_dir_stacked_spinner.spinner.is_spinning
+        expected_items = [str(x) for x in range(count)]
+        assert combo_box.count() == len(expected_items)
+        for i in range(combo_box.count()):
+            assert combo_box.itemText(i) == expected_items[i]
+
+
+    def test_seg2_channel_count_set(self, qtbot: QtBot, test_env: TestEnvironment) -> None:
+        # Arrange
+        test_env.view.seg2_directory_select.button.click()
+        # Simulate the model's channel count being set
+        count: int = 4
+        combo_box: QComboBox = test_env.view.seg2_image_channel_combo
+
+        # Act
+        test_env.model.set_channel_count(CurationImageType.SEG2, count)
+
+        # Assert
+        assert not test_env.view.seg2_dir_stacked_spinner.spinner.is_spinning
+        expected_items = [str(x) for x in range(count)]
+        assert combo_box.count() == len(expected_items)
+        for i in range(combo_box.count()):
+            assert combo_box.itemText(i) == expected_items[i]
 
 
 ### View + Model Integration Tests ------------------------------------------------------------------------
