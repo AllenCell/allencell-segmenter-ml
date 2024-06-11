bcbacb4ef92a6cdf48b0dde62517ffea047c942e
chrishu <chrishu@alleninstitute.org>
2024-06-11 08:48:20 -0400

    lint

--- src/allencell_ml_segmenter/main/experiments_model.py
+++ src/allencell_ml_segmenter/main/experiments_model.py
@@ -30,8 +30,10 @@ class ExperimentsModel(IExperimentsModel):
             experiment
         ) in self.user_settings.get_user_experiments_path().iterdir():
             if (
-                experiment.is_dir() 
-                and Path(experiment / "checkpoints").exists() # Heuristic for checking if dir is a cyto-dl experiment
+                experiment.is_dir()
+                and Path(
+                    experiment / "checkpoints"
+                ).exists()  # Heuristic for checking if dir is a cyto-dl experiment
                 and experiment not in self.experiments
                 and not experiment.name.startswith(".")
             ):
