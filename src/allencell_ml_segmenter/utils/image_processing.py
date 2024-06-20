d7e3b8ffcbd4bb1d1e3a9388c47254940857886e
brian.kim <yrkim98@uw.edu>
2024-06-20 15:33:23 -0700

    lint

--- src/allencell_ml_segmenter/utils/image_processing.py
+++ src/allencell_ml_segmenter/utils/image_processing.py
@@ -13,5 +13,3 @@ class ImageProcessing:
     def set_all_nonzero_values_to(image: numpy.ndarray, value: int) -> None:
         # set all nonzero values in this :image to :value in-place
         image[image > 0] = value
-
-
