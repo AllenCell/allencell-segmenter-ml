4a32b93e365bdbc01b21989a92da391157e34911
brian.kim <yrkim98@uw.edu>
2024-06-21 13:44:16 -0700

    seg images are formatted for napari labels layer when np.data is extracted

--- src/allencell_ml_segmenter/core/image_data_extractor/aics_image_data_extractor.py
+++ src/allencell_ml_segmenter/core/image_data_extractor/aics_image_data_extractor.py
@@ -1,9 +1,13 @@
 from pathlib import Path
+from typing import Optional
+
 from allencell_ml_segmenter.core.image_data_extractor import (
     IImageDataExtractor,
     ImageData,
 )
 from aicsimageio.aics_image import AICSImage
+
+from allencell_ml_segmenter.utils.image_processing import set_all_nonzero_values_to
 
 
 class AICSImageDataExtractor(IImageDataExtractor):
@@ -19,18 +23,22 @@ class AICSImageDataExtractor(IImageDataExtractor):
         channel: int = 0,
         dims: bool = True,
         np_data: bool = True,
-    ) -> ImageData:
+        seg: Optional[int] = None) -> ImageData:
+
         aics_img: AICSImage = AICSImage(img_path)
+
+        numpy_data = None
+        if np_data:
+            numpy_data = aics_img.get_image_dask_data("ZYX", C=channel).compute()
+            if seg:
+                set_all_nonzero_values_to(numpy_data, seg)
+
         return ImageData(
             aics_img.dims.X if dims else None,
             aics_img.dims.Y if dims else None,
             aics_img.dims.Z if dims else None,
             aics_img.dims.C if dims else None,
-            (
-                aics_img.get_image_dask_data("ZYX", C=channel).compute()
-                if np_data
-                else None
-            ),
+            numpy_data,
             img_path,
         )
 
