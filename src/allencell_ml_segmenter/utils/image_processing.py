0b57874ccb292af1542095229556cb4a51032a87
brian.kim <yrkim98@uw.edu>
2024-06-24 11:54:38 -0700

    lint

--- src/allencell_ml_segmenter/utils/image_processing.py
+++ src/allencell_ml_segmenter/utils/image_processing.py
@@ -1,7 +1,9 @@
 import numpy
 
 
-def set_all_nonzero_values_to(image: numpy.ndarray, value: int) -> numpy.ndarray:
+def set_all_nonzero_values_to(
+    image: numpy.ndarray, value: int
+) -> numpy.ndarray:
     copy = numpy.copy(image)
     # set all nonzero values in this :image to :value in-place
     copy[copy > 0] = value
