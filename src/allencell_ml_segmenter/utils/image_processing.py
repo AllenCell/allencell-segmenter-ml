fd4e127af990011a4b705cb2c1229fa8dd4aa4ea
brian.kim <yrkim98@uw.edu>
2024-06-24 11:42:16 -0700

    make function not mutate args

--- src/allencell_ml_segmenter/utils/image_processing.py
+++ src/allencell_ml_segmenter/utils/image_processing.py
@@ -1,6 +1,8 @@
 import numpy
 
 
-def set_all_nonzero_values_to(image: numpy.ndarray, value: int) -> None:
+def set_all_nonzero_values_to(image: numpy.ndarray, value: int) -> numpy.ndarray:
+    copy = numpy.copy(image)
     # set all nonzero values in this :image to :value in-place
-    image[image > 0] = value
+    copy[copy > 0] = value
+    return copy
