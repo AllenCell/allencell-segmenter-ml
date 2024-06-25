fd386223c40b2bb1bc2872231e6b6fac85168030
aicsgithub <aicsgithub@alleninstitute.org>
2024-06-25 21:28:17 +0000

    Bump version 0.1.7 -> 0.1.8

--- src/allencell_ml_segmenter/__init__.py
+++ src/allencell_ml_segmenter/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 from allencell_ml_segmenter.napari.napari_reader import napari_get_reader
 from allencell_ml_segmenter.napari.sample_data import make_sample_data
