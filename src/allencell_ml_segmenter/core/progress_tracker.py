070802354edb1f14025e7ef314ad494dec736d3d
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-06-25 15:30:16 -0700

    transition works

--- src/allencell_ml_segmenter/core/progress_tracker.py
+++ src/allencell_ml_segmenter/core/progress_tracker.py
@@ -1,5 +1,10 @@
 from abc import abstractmethod
+from qtpy.QtCore import QObject, Signal
 
+class ProgressTrackerSignals(QObject):
+    progress_changed: Signal = Signal(int)
+    label_text_changed: Signal = Signal(str)
+    progress_max_changed: Signal = Signal(int)
 
 class ProgressTracker:
     """
@@ -20,12 +25,17 @@ class ProgressTracker:
         self._progress_maximum: int = progress_maximum
         self._progress: int = progress_minimum
         self._label_text: str = label_text
+        self.signals: ProgressTrackerSignals = ProgressTrackerSignals()
 
     def get_progress_minimum(self) -> int:
         return self._progress_minimum
 
     def get_progress_maximum(self) -> int:
         return self._progress_maximum
+
+    def set_progress_maximum(self, maximum: int) -> None:
+        self._progress_maximum = maximum
+        self.signals.progress_max_changed.emit(self._progress_maximum)
 
     def get_progress(self) -> int:
         return self._progress
@@ -46,12 +56,14 @@ class ProgressTracker:
             )
 
         self._progress = progress
+        self.signals.progress_changed.emit(self._progress)
 
     def get_label_text(self) -> str:
         return self._label_text
 
     def set_label_text(self, label_text: str) -> None:
         self._label_text = label_text
+        self.signals.label_text_changed.emit(self._label_text)
 
     @abstractmethod
     def start_tracker(self) -> None:
