c9a4764811ece0b91075a8143dc35af39798170a
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-06-28 13:55:11 -0700

    image selection widget tests

--- src/allencell_ml_segmenter/_tests/training/test_image_selection_widget.py
+++ src/allencell_ml_segmenter/_tests/training/test_image_selection_widget.py
@@ -16,7 +16,7 @@ from allencell_ml_segmenter._tests.fakes.fake_experiments_model import (
 from allencell_ml_segmenter.training.image_selection_widget import (
     ImageSelectionWidget,
 )
-from allencell_ml_segmenter.training.training_model import TrainingModel
+from allencell_ml_segmenter.training.training_model import TrainingModel, TrainingImageType
 
 
 MOCK_PATH: str = "/path/to/file"
@@ -45,7 +45,7 @@ def image_selection_widget(
     """
     Fixture that creates an instance of ImageSelectionWidget for testing.
     """
-    return ImageSelectionWidget(training_model, FakeExperimentsModel())
+    return ImageSelectionWidget(training_model, experiments_model)
 
 
 # decorator used to stub QFileDialog and avoid nested context managers
@@ -71,21 +71,87 @@ def test_set_images_directory(
     # ASSERT
     assert training_model.get_images_directory() == Path(MOCK_PATH)
 
+def test_combos_update_when_num_channels_set(
+        qtbot: QtBot,
+        image_selection_widget: ImageSelectionWidget,
+        training_model: TrainingModel,
+    ) -> None:
+    # ASSERT (sanity check)
+    assert image_selection_widget._raw_channel_combo_box.count() == 0
+    assert image_selection_widget._seg1_channel_combo_box.count() == 0
+    assert image_selection_widget._seg2_channel_combo_box.count() == 0
 
-def test_set_channel_index(
-    image_selection_widget: ImageSelectionWidget,
-    training_model: TrainingModel,
-) -> None:
-    """
-    Tests that the slot connected to the image channel QComboBox properly sets the image channel index field.
-    """
-    # ARRANGE - add arbitrary channel index options to the QComboBox, since it does not come with default choices
-    mock_choices: List[str] = [str(i) for i in range(10)]
-    image_selection_widget._channel_combo_box.addItems(mock_choices)
+    assert image_selection_widget._raw_channel_combo_box.currentIndex() == -1
+    assert image_selection_widget._seg1_channel_combo_box.currentIndex() == -1
+    assert image_selection_widget._seg2_channel_combo_box.currentIndex() == -1
 
-    for i in range(10):
-        # ACT
-        image_selection_widget._channel_combo_box.setCurrentIndex(i)
+    assert training_model.get_selected_channel(TrainingImageType.RAW) == None
+    assert training_model.get_selected_channel(TrainingImageType.SEG1) == None
+    assert training_model.get_selected_channel(TrainingImageType.SEG2) == None
 
-        # ASSERT
-        assert training_model.get_channel_index() == i
+    # ACT (image selection widget should react to this event)
+    training_model.set_all_num_channels({
+        TrainingImageType.RAW: 1,
+        TrainingImageType.SEG1: 2,
+        TrainingImageType.SEG2: 3,
+    })
+
+    # ASSERT
+    assert image_selection_widget._raw_channel_combo_box.count() == 1
+    assert image_selection_widget._seg1_channel_combo_box.count() == 2
+    assert image_selection_widget._seg2_channel_combo_box.count() == 3
+
+    assert image_selection_widget._raw_channel_combo_box.currentIndex() == 0
+    assert image_selection_widget._seg1_channel_combo_box.currentIndex() == 0
+    assert image_selection_widget._seg2_channel_combo_box.currentIndex() == 0
+
+    assert training_model.get_selected_channel(TrainingImageType.RAW) == 0
+    assert training_model.get_selected_channel(TrainingImageType.SEG1) == 0
+    assert training_model.get_selected_channel(TrainingImageType.SEG2) == 0
+
+def test_combos_index_controls_model_state(
+        qtbot: QtBot,
+        image_selection_widget: ImageSelectionWidget,
+        training_model: TrainingModel,
+    ) -> None:
+    # ARRANGE
+    training_model.set_all_num_channels({
+        TrainingImageType.RAW: 4,
+        TrainingImageType.SEG1: 4,
+        TrainingImageType.SEG2: 4,
+    })
+
+    # ACT
+    image_selection_widget._raw_channel_combo_box.setCurrentIndex(1)
+    image_selection_widget._seg1_channel_combo_box.setCurrentIndex(2)
+    image_selection_widget._seg2_channel_combo_box.setCurrentIndex(3)
+
+    # ASSERT
+    assert training_model.get_selected_channel(TrainingImageType.RAW) == 1
+    assert training_model.get_selected_channel(TrainingImageType.SEG1) == 2
+    assert training_model.get_selected_channel(TrainingImageType.SEG2) == 3
+
+def test_reset_num_channels_to_none(
+        qtbot: QtBot,
+        image_selection_widget: ImageSelectionWidget,
+        training_model: TrainingModel,
+    ) -> None:
+    # ARRANGE
+    training_model.set_all_num_channels({
+        TrainingImageType.RAW: 4,
+        TrainingImageType.SEG1: 4,
+        TrainingImageType.SEG2: 4,
+    })
+
+    # ACT
+    training_model.set_all_num_channels({
+        TrainingImageType.RAW: None,
+        TrainingImageType.SEG1: None,
+        TrainingImageType.SEG2: None,
+    })
+
+    # ASSERT
+    assert training_model.get_selected_channel(TrainingImageType.RAW) == None
+    assert training_model.get_selected_channel(TrainingImageType.SEG1) == None
+    assert training_model.get_selected_channel(TrainingImageType.SEG2) == None
+
