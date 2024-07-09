ec58bf5fbc7a4d516fe0616e168e86f2e42b4dd3
brian.kim <yrkim98@uw.edu>
2024-07-09 16:16:32 -0700

    clean up

--- src/allencell_ml_segmenter/_tests/utils/zip_file/test_zip_file_manager.py
+++ src/allencell_ml_segmenter/_tests/utils/zip_file/test_zip_file_manager.py
@@ -1,10 +1,11 @@
+import allencell_ml_segmenter
+from allencell_ml_segmenter.utils.zip_file import (
+    ZipFileManager,
+    IZipFileManager,
+)
+
 from pathlib import Path
 from unittest.mock import Mock, mock_open, patch, create_autospec
-from zipfile import ZipFile
-
-import allencell_ml_segmenter
-from allencell_ml_segmenter.utils.file_utils import FileUtils
-from allencell_ml_segmenter.utils.zip_file import ZipFileManager
 
 
 def test_write_to_file() -> None:
@@ -18,7 +19,7 @@ def test_write_to_file() -> None:
         / "test_zip.zip"
     )
     open_file_mock: Mock = mock_open()
-    file_manager = ZipFileManager.global_instance()
+    file_manager: IZipFileManager = ZipFileManager.global_instance()
 
     # Act
     with patch(
@@ -32,11 +33,11 @@ def test_write_to_file() -> None:
     open_file_mock.return_value.write.assert_called_once_with(to_write)
 
 
-def test_unzip_zipped_file_and_delete_zip():
+def test_unzip_zipped_file_and_delete_zip() -> None:
     # Arrange
-    file_manager = ZipFileManager.global_instance()
-    mock_extract_all = Mock()
-    mock_path = create_autospec(Path)
+    file_manager: IZipFileManager = ZipFileManager.global_instance()
+    mock_extract_all: Mock = Mock()
+    mock_path: Mock = create_autospec(Path)
     mock_path.parent = "parent"
     with patch(
         "allencell_ml_segmenter.utils.zip_file.zip_file_manager.ZipFile"
