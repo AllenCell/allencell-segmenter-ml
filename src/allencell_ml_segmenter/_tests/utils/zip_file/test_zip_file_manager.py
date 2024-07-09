b343e2c2f97df70acf65ebc3036fb5b81f48aa47
brian.kim <yrkim98@uw.edu>
2024-07-09 14:44:55 -0700

    zipfile tests

--- src/allencell_ml_segmenter/_tests/utils/zip_file/test_zip_file_manager.py
+++ src/allencell_ml_segmenter/_tests/utils/zip_file/test_zip_file_manager.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from unittest.mock import Mock, mock_open, patch
+from unittest.mock import Mock, mock_open, patch, create_autospec
 from zipfile import ZipFile
 
 import allencell_ml_segmenter
@@ -27,3 +27,23 @@ def test_write_to_file() -> None:
     # Assert
     open_file_mock.assert_called_with(test_path, "wb")
     open_file_mock.return_value.write.assert_called_once_with(to_write)
+
+def test_unzip_zipped_file_and_delete_zip():
+    # Arrange
+    file_manager = ZipFileManager.global_instance()
+    mock_extract_all = Mock()
+    mock_path = create_autospec(Path)
+    mock_path.parent = "parent"
+    with patch("allencell_ml_segmenter.utils.zip_file.zip_file_manager.ZipFile") as zipfile_mock:
+        zipfile_mock.return_value.__enter__.return_value.extractall = mock_extract_all
+
+        # Act
+        file_manager.unzip_zipped_file_and_delete_zip(mock_path)
+
+    # Assert
+    mock_extract_all.assert_called_once_with(mock_path.parent)
+    mock_path.unlink.assert_called_once()
+
+
+
+
