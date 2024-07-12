15942bdbfa942d34c373c51211ddeb0872ded76a
brian.kim <yrkim98@uw.edu>
2024-07-12 09:13:26 -0700

    zipfilemanager no longer singleton

--- src/allencell_ml_segmenter/utils/zip_file/fake_zip_file_manager.py
+++ src/allencell_ml_segmenter/utils/zip_file/fake_zip_file_manager.py
@@ -8,21 +8,15 @@ class FakeZipFileManager(IZipFileManager):
     """
     FakeZipFileManager for testing
     """
-
-    _instance: Optional = None
-    # zip files that have been written, keys- path, values- content written
-    written_zip_files: dict[Path, bytes] = {}
-    # list of unzipped files
-    unzipped_files: list[Path] = []
+    def __init__(self):
+        super().__init__()
+        # zip files that have been written, keys- path, values- content written
+        self.written_zip_files: dict[Path, bytes] = {}
+        # list of unzipped files
+        self.unzipped_files: list[Path] = []
 
     def write_zip_file(self, path: Path, contents: bytes) -> None:
         self.written_zip_files[path] = contents
 
     def unzip_zipped_file_and_delete_zip(self, path_to_zipped: Path) -> None:
         self.unzipped_files.append(path_to_zipped)
-
-    @classmethod
-    def global_instance(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
