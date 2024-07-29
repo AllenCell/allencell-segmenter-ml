c29850225d6f1b5b009458087f01cae041952154
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-07-29 15:45:57 -0700

    simple fix for pred problem

--- src/allencell_ml_segmenter/_tests/prediction/test_view.py
+++ src/allencell_ml_segmenter/_tests/prediction/test_view.py
@@ -84,3 +84,42 @@ def test_show_results(main_model: MainModel) -> None:
     assert fake_viewer.contains_layer("[seg] output_1.tiff")
     assert fake_viewer.contains_layer("[raw] output_2.tiff")
     assert fake_viewer.contains_layer("[seg] output_2.tiff")
+
+
+def test_show_results_non_empty_folder(main_model: MainModel) -> None:
+    """
+    Testing that only the new images in a folder will be shown after prediction.
+    """
+    # ARRANGE
+    prediction_model: PredictionModel = PredictionModel()
+    prediction_model.set_output_directory(
+        Path(allencell_ml_segmenter.__file__).parent
+        / "_tests"
+        / "test_files"
+        / "output_test_folder_extra"
+    )
+    prediction_model.set_prediction_input_mode(
+        PredictionInputMode.FROM_NAPARI_LAYERS
+    )
+    prediction_model.set_selected_paths(
+        [Path("output_3.tiff"), Path("output_4.tiff")]
+    )
+    fake_viewer: FakeViewer = FakeViewer()
+
+    prediction_view: PredictionView = PredictionView(
+        main_model,
+        prediction_model,
+        fake_viewer,
+        img_data_extractor=FakeImageDataExtractor.global_instance(),
+    )
+
+    # ACT
+    prediction_view.showResults()
+
+    # ASSERT
+    assert len(fake_viewer.get_all_labels()) == 2
+    assert len(fake_viewer.get_all_images()) == 2
+    assert fake_viewer.contains_layer("[raw] output_3.tiff")
+    assert fake_viewer.contains_layer("[seg] output_3.tiff")
+    assert fake_viewer.contains_layer("[raw] output_4.tiff")
+    assert fake_viewer.contains_layer("[seg] output_4.tiff")
