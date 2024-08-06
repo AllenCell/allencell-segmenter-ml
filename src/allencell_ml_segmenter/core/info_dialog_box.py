df209d413d5004ae8739e653ff8e59e78e736baf
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-08-06 11:37:05 -0700

    partway through core

--- src/allencell_ml_segmenter/core/info_dialog_box.py
+++ src/allencell_ml_segmenter/core/info_dialog_box.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from qtpy.QtWidgets import (
     QDialog,
     QWidget,
@@ -13,15 +14,18 @@ class InfoDialogBox(QDialog):
     Warning message Widget with a yellow warning sign icon on the left.
     """
 
-    def __init__(self, message: str, parent: QWidget = None):
+    def __init__(self, message: str, parent: Optional[QWidget] = None):
         super().__init__(parent=parent)
-        self.setLayout(QHBoxLayout())
-
-        self.layout().addStretch()
-        self.layout().addWidget(QLabel(message))
-        self.layout().addStretch()
+        layout: QHBoxLayout = QHBoxLayout()
+        self.setLayout(layout)
+        layout.addStretch()
+        layout.addWidget(QLabel(message))
+        layout.addStretch()
         btns = QVBoxLayout()
         self.close_btn = QPushButton("Close")
-        self.close_btn.clicked.connect(self.close)
+        self.close_btn.clicked.connect(self._on_close_clicked)
         btns.addWidget(self.close_btn)
-        self.layout().addLayout(btns)
+        layout.addLayout(btns)
+    
+    def _on_close_clicked(self) -> None:
+        self.close()
