2c06128ddeb4b7470820004a691cc41978c7da77
brian.kim <yrkim98@uw.edu>
2024-09-05 16:11:17 -0700

    test

--- post_install.py
+++ post_install.py
@@ -3,7 +3,8 @@ import subprocess
 
 def install_torch():
     if sys.platform.startswith('win') or sys.platform.startswith('linux'):
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'torch==2.0.1+cu118', "--index-url", "https://download.pytorch.org/whl/cu118"])
+        subprocess.check_call([sys.executable, '-m', 'pip', 'uninstall', 'torch', "torchvision"])
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'torch==2.0.1+cu118', "torchvision" "--index-url", "https://download.pytorch.org/whl/cu118"])
 
 if __name__ == '__main__':
     install_torch()
