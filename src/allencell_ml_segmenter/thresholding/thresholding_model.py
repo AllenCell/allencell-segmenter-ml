ffc6a886dce786abab2af27777f8263e13b4aceb
brian.kim <yrkim98@uw.edu>
2024-12-05 15:41:23 -0800

    orderedict changes discussed last week

--- src/allencell_ml_segmenter/thresholding/thresholding_model.py
+++ src/allencell_ml_segmenter/thresholding/thresholding_model.py
@@ -1,4 +1,9 @@
 from typing import Optional
+from collections import OrderedDict
+
+import numpy as np
+from napari.layers import Layer
+
 from allencell_ml_segmenter.core.event import Event
 from allencell_ml_segmenter.core.publisher import Publisher
 
@@ -21,6 +26,9 @@ class ThresholdingModel(Publisher):
         self._thresholding_value_selected: int = THRESHOLD_DEFAULT
         self._is_autothresholding_enabled: bool = False
         self._autothresholding_method: str = AVAILABLE_AUTOTHRESHOLD_METHODS[0]
+        self._original_layers_in_viewer: Optional[
+            OrderedDict[str, np.ndarray]
+        ] = None  # Orderedict of layers when thresholding starts, in original order.
 
     def set_thresholding_value(self, value: int) -> None:
         """
@@ -76,3 +84,28 @@ class ThresholdingModel(Publisher):
 
     def dispatch_save_thresholded_images(self) -> None:
         self.dispatch(Event.ACTION_SAVE_THRESHOLDING_IMAGES)
+
+    def set_original_layers(self, layer_list: list[Layer]) -> None:
+        self._original_layers_in_viewer = {}
+        for layer in layer_list:
+            self._original_layers_in_viewer[layer.name] = layer.data
+
+    def get_original_layers(self) -> Optional[OrderedDict[str, np.ndarray]]:
+        return self._original_layers_in_viewer
+
+    def get_layers_to_threshold(
+        self, only_seg_layers: bool
+    ) -> OrderedDict[str, np.ndarray]:
+        if self._original_layers_in_viewer is None:
+            raise ValueError(
+                "Check original layers in model for None before calling get_layers_to_threshold"
+            )
+
+        if only_seg_layers:
+            return OrderedDict(
+                (key, value)
+                for key, value in self._original_layers_in_viewer.items()
+                if key.startswith("[seg]")
+            )
+
+        return self._original_layers_in_viewer
