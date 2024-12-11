1c6c08f72278d33c20d210e5fbafc29590ba5013
yrkim98 <yrkim98@uw.edu>
2024-12-11 13:22:27 -0800

    manual version bump to 1.0.0rc1

--- src/allencell_ml_segmenter/__init__.py
+++ src/allencell_ml_segmenter/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.0rc0"
+__version__ = "1.0.0rc1"
 
 from allencell_ml_segmenter.napari.napari_reader import napari_get_reader
 from allencell_ml_segmenter.napari.sample_data import make_sample_data
