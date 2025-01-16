aff12aab04cbfc8feac4ff70d87bef032c6a4e7e
brian.kim <yrkim98@uw.edu>
2025-01-16 09:28:14 -0800

    lint, fix tests

--- src/allencell_ml_segmenter/main/viewer.py
+++ src/allencell_ml_segmenter/main/viewer.py
@@ -193,21 +193,24 @@ class Viewer(IViewer):
             return Path(layer.metadata["source_path"])
         return None
 
-    def add_segmentation_labels(self, masked_data: np.ndarray, name: str, prob_map: np.ndarray) -> None:
+    def add_segmentation_labels(
+        self, masked_data: np.ndarray, name: str, prob_map: np.ndarray
+    ) -> None:
         """
         Add a segmentation labels layer and save probability mapping in the layer metadata.
         """
         self.add_labels(
             masked_data,
             name=name,
-            metadata={
-                "prob_map": prob_map
-            },
+            metadata={"prob_map": prob_map},
         )
 
     def get_all_segmentation_labels(self) -> list[LabelsLayer]:
         """
         Get all segmentation labels layers that currently exist in the viewer.
         """
-        return [layer for layer in self.get_layers() if "prob_map" in layer.metadata]
-
+        return [
+            layer
+            for layer in self.get_layers()
+            if "prob_map" in layer.metadata
+        ]
