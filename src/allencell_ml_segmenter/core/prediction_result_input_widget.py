c89aecd9aef203ad52eb587dcac58deed0362e22
brian.kim <yrkim98@uw.edu>
2025-01-30 11:15:31 -0800

    maintain previous selections

--- src/allencell_ml_segmenter/core/prediction_result_input_widget.py
+++ src/allencell_ml_segmenter/core/prediction_result_input_widget.py
@@ -24,12 +24,15 @@ class PredictionResultListWidget(FileInputWidget):
         self._prediction_layers: list[LabelsLayer] = []
 
     def _update_layer_list(self) -> None:
+        previous_selections: list[int] = self._image_list.get_checked_rows()
         self._image_list.clear()
         self._prediction_layers = self._viewer.get_all_segmentation_labels()
-        for prediction_output_layer in self._prediction_layers:
+        for idx, prediction_output_layer in enumerate(self._prediction_layers):
             self._image_list.add_item(
                 prediction_output_layer.name,
+                set_checked=idx in previous_selections
             )
+
 
     def process_checked_signal(self, row: int, state: Qt.CheckState) -> None:
         if self._model.get_input_mode() == InputMode.FROM_NAPARI_LAYERS:
