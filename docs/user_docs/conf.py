1cdd7077e882bdfedd86f1ec857f2e35507ae461
brian.kim <yrkim98@uw.edu>
2025-09-19 10:13:31 -0700

    fix manual bumpver

--- docs/user_docs/conf.py
+++ docs/user_docs/conf.py
@@ -10,7 +10,7 @@
 project = "Segmenter ML Plugin for napari"
 copyright = "2024, Allen Institute for Cell Science"
 author = "Segmenter ML plugin team"
-release = "1.0.1dev0"
+release = "1.0.1rc0"
 
 
 # -- General configuration ---------------------------------------------------
